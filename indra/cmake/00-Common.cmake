5452360c21c19d72f9ab9441afb4f99b56bd3df3
Kitty Barnett <none@none>
2010-08-22 05:57:21 +0200

    [Viewer/Build] - added: imported SNOW-788 - Fix boost lib detection with Visual Studio to allow multiple VS versions
    --HG--
    branch : Viewer-Build

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -78,6 +78,20 @@ if (WINDOWS)
   # configure win32 API for windows XP+ compatibility
   set(WINVER "0x0501" CACHE STRING "Win32 API Target version (see http://msdn.microsoft.com/en-us/library/aa383745%28v=VS.85%29.aspx)")
   add_definitions("/DWINVER=${WINVER}" "/D_WIN32_WINNT=${WINVER}")
+    
+  # Various libs are compiler specific, generate some variables here we can just use
+  # when we require them instead of reimplementing the test each time.
+  if (MSVC71)
+	set(MSVC_DIR 7.1)
+	set(MSVC_SUFFIX 71)
+  elseif (MSVC80)
+	set(MSVC_DIR 8.0)
+	set(MSVC_SUFFIX 80)
+  elseif (MSVC90)
+	set(MSVC_DIR 9.0)
+	set(MSVC_SUFFIX 90)
+  endif (MSVC71)
+  
 endif (WINDOWS)
 
 
