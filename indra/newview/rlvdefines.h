aa4ac83eef104821978060de6adff99051edac46
Kitty Barnett <none@none>
2010-08-22 11:02:03 +0200

    Incremented version number to RLVa-1.2.1(a)
    --HG--
    branch : RLVa

--- indra/newview/rlvdefines.h
+++ indra/newview/rlvdefines.h
@@ -74,8 +74,8 @@ const S32 RLV_VERSION_BUILD = 0;
 // Implementation version
 const S32 RLVa_VERSION_MAJOR = 1;
 const S32 RLVa_VERSION_MINOR = 2;
-const S32 RLVa_VERSION_PATCH = 0;
-const S32 RLVa_VERSION_BUILD = 7;
+const S32 RLVa_VERSION_PATCH = 1;
+const S32 RLVa_VERSION_BUILD = 0;
 
 // Uncomment before a final release
 //#define RLV_RELEASE
