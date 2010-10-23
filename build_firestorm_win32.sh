22870bdeae90af12ed4426e87bdb811c232f3368
Arrehn <none@none>
2010-10-23 14:00:02 -0400

    win32 build script, intended to be run in cygwin. Work In Progress

---
+++ build_firestorm_win32.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+###
+### Constants
+###
+
+TRUE=0 # Map the shell's idea of truth to a variable for better documentation
+FALSE=1
+LOG="`pwd`/logs/build_linux.log"
+
+###
+### Global Variables
+###
+
+WANTS_CLEAN=$TRUE
+WANTS_CONFIG=$TRUE
+WANTS_BUILD=$TRUE
+WANTS_PACKAGE=$TRUE
+
+###
+### Helper Functions
+###
+
+if [ "$1" == "--rebuild" ] ; then
+	echo "rebuilding..."
+	WANTS_CLEAN=$FALSE
+	WANTS_CONFIG=$FALSE
+fi
+
+###
+###  Main Logic
+### 
+
+path=$WINPATH:/usr/local/bin:/usr/bin:/bin
+pushd indra
+if [ $WANTS_CLEAN -eq $TRUE ] ; then
+	cmd /c "develop.py clean"
+	find . -name "*.pyc" -exec rm {} \;
+fi
+
+
+if [ $WANTS_CONFIG -eq $TRUE ] ; then
+	mkdir -p ../logs > /dev/null 2>&1
+	cmd /c "develop.py -G vc80 configure -DLL_TESTS:BOOL=OFF"
+fi
+
+exit 1
+
+if [ $WANTS_BUILD -eq $TRUE ] ; then
+	echo "Building in progress... Check $LOG for verbose status"
+	cmd /c "develop.py build"
+	echo "Complete"
+fi
+popd
+
