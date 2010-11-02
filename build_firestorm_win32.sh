687b33ca55b02bf12090d1099480bcf349aaa337
Arrehn <none@none>
2010-11-01 23:21:42 -0400

    Usability improvements to win32 build

--- build_firestorm_win32.sh
+++ build_firestorm_win32.sh
@@ -40,7 +40,7 @@ fi
 
 pushd indra
 if [ $WANTS_CLEAN -eq $TRUE ] ; then
-	cmd /c "develop.py clean"
+	$PYTHON develop.py clean
 	find . -name "*.pyc" -exec rm {} \;
 fi
 
@@ -48,10 +48,10 @@ fi
 if [ $WANTS_CONFIG -eq $TRUE ] ; then
 	mkdir -p ../logs > /dev/null 2>&1
 	$PYTHON develop.py -G vc80 -t Release configure -DPACKAGE:BOOL=ON -DLL_TESTS:BOOL=OFF  2>&1 | tee $LOG
-	mkdir -p build_vc80/sharedlibs/RelWithDebInfo
-	mkdir -p build_vc80/sharedlibs/Release
-	cp fmod.dll ./build_vc80/sharedlibs/RelWithDebInfo
-	cp fmod.dll ./build_vc80/sharedlibs/Release
+	mkdir -p build-vc80/sharedlibs/RelWithDebInfo
+	mkdir -p build-vc80/sharedlibs/Release
+	cp fmod.dll ./build-vc80/sharedlibs/RelWithDebInfo
+	cp fmod.dll ./build-vc80/sharedlibs/Release
 fi
 
 if [ $WANTS_BUILD -eq $TRUE ] ; then
@@ -63,7 +63,7 @@ if [ $WANTS_BUILD -eq $TRUE ] ; then
 
 
 	echo "Building in progress... Check $LOG for verbose status"
-	$PYTHON develop.py -G vc80 -t Release build  2>&1 | tee -a $LOG
+	$PYTHON develop.py -G vc80 -t Release build  2>&1 | tee -a $LOG | grep Build
 	echo "Complete"
 fi
 popd
