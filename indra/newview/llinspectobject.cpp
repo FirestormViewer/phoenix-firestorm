6cad3862dd8a5eb0a7926a04442c37a61248f8f8
Kitty Barnett <develop@catznip.com>
2010-11-02 18:15:42 +0100

    - merge : anonymize names on the object inspector when @shownames=n restricted
    --HG--
    branch : RLVa

--- indra/newview/llinspectobject.cpp
+++ indra/newview/llinspectobject.cpp
@@ -486,25 +486,16 @@ void LLInspectObject::updateCreator(LLSelectNode* nodep)
 		// a clickable link		
 		// Objects cannot be created by a group, so use agent URL format
 		LLUUID creator_id = nodep->mPermissions->getCreator();
-// [RLVa:KB] - Checked: 2010-08-25 (RLVa-1.2.1b) | Added: RLVa-1.2.1b
+//		std::string creator_url = 
+//			LLSLURL("agent", creator_id, "about").getSLURLString();
+// [RLVa:KB] - Checked: 2010-11-02 (RLVa-1.2.2a) | Modified: RLVa-1.2.2a
 		// Only anonymize the creator if they're also the owner or if they're a nearby avie
-		if ( (gRlvHandler.hasBehaviour(RLV_BHVR_SHOWNAMES)) &&
-			 ((nodep->mPermissions->getOwner() == creator_id) || (RlvUtil::isNearbyAgent(creator_id))) )
-		{
-			// TODO-RLVa: [RLVa-1.2.2] We need to put a callback here in case the name hasn't previously resolved
-			std::string strFullName;
-			args["[CREATOR]"] = (gCacheName->getFullName(creator_id, strFullName)) ? RlvStrings::getAnonym(strFullName) 
-			                                                                       : LLTrans::getString("Unknown");
-		}
-		else
-		{
+		bool fRlvHideCreator = (gRlvHandler.hasBehaviour(RLV_BHVR_SHOWNAMES)) &&
+			 ((nodep->mPermissions->getOwner() == creator_id) || (RlvUtil::isNearbyAgent(creator_id)));
+		std::string creator_url = 
+			LLSLURL("agent", creator_id, (!fRlvHideCreator) ? "about" : "rlvanonym").getSLURLString();
 // [/RLVa:KB]
-			std::string creator_url = 
-				LLSLURL("agent", creator_id, "about").getSLURLString();
-			args["[CREATOR]"] = creator_url;
-// [RLVa:KB] - Checked: 2010-08-25 (RLVa-1.2.1b) | Added: RLVa-1.2.1b
-		}
-// [/RLVa:KB]
+		args["[CREATOR]"] = creator_url;
 
 		// created by one user but owned by another
 		std::string owner_url;
@@ -518,20 +509,10 @@ void LLInspectObject::updateCreator(LLSelectNode* nodep)
 		else
 		{
 			owner_id = nodep->mPermissions->getOwner();
-// [RLVa:KB] - Checked: 2010-08-25 (RLVa-1.2.1b) | Added: RLVa-1.2.1b
-			if (gRlvHandler.hasBehaviour(RLV_BHVR_SHOWNAMES))
-			{
-				// TODO-RLVa: [RLVa-1.2.2] We need to put a callback here in case the name hasn't previously resolved
-				std::string strFullName;
-				owner_url = (gCacheName->getFullName(owner_id, strFullName)) ? RlvStrings::getAnonym(strFullName)
-				                                                             : LLTrans::getString("Unknown");
-			}
-			else
-			{
-// [/RLVa:KB]
-				owner_url = LLSLURL("agent", owner_id, "about").getSLURLString();
-// [RLVa:KB] - Checked: 2010-08-25 (RLVa-1.2.1b) | Added: RLVa-1.2.1b
-			}
+//			owner_url = LLSLURL("agent", owner_id, "about").getSLURLString();
+// [RLVa:KB] - Checked: 2010-11-02 (RLVa-1.2.2a) | Modified: RLVa-1.2.2a
+			bool fRlvHideOwner = (gRlvHandler.hasBehaviour(RLV_BHVR_SHOWNAMES));
+			owner_url = LLSLURL("agent", owner_id, (!fRlvHideOwner) ? "about" : "rlvanonym").getSLURLString();
 // [/RLVa:KB]
 		}
 		args["[OWNER]"] = owner_url;
