970f6f8b1cbcb6a33d2bfb62052025ddc1b249ff
Arrehn <none@none>
2010-11-04 14:35:03 -0700

    RLVa merges - make tests complete

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -30,7 +30,7 @@
 const S32 LL_VERSION_MAJOR = 2;
 const S32 LL_VERSION_MINOR = 4;
 const S32 LL_VERSION_PATCH = 0;
-const S32 LL_VERSION_BUILD = 36;
+const S32 LL_VERSION_BUILD = 40;
 
 const char * const LL_CHANNEL = "Firestorm Developer";
 
