743b34a70a238dadb8e33b5786e50ecf33b9c249
Tonya Souther <tonya.souther@gmail.com>
2010-11-12 09:31:44 -0600

    Changing build number to Mercurial revision number in OS X build. Dunno how it's done on Windows; will do Linux script momentarily.

--- build_firestorm_macosx.sh
+++ build_firestorm_macosx.sh
@@ -25,6 +25,9 @@ if [ "$1" == "--rebuild" ] ; then
 	echo "rebuilding..."
 	WANTS_CLEAN=$FALSE
 	WANTS_CONFIG=$FALSE
+elif [ "$1" == "--config" ] ; then
+	echo "configuring..."
+	WANTS_BUILD=$FALSE
 fi
 
 ###
@@ -35,7 +38,7 @@ if [ ! -d `dirname $LOG` ] ; then
 	mkdir -p `dirname $LOG`
 fi
 
-pushd indra
+pushd indra >/dev/null
 if [ $WANTS_CLEAN -eq $TRUE ] ; then
 	./develop.py clean
 	find . -name "*.pyc" -exec rm {} \;
@@ -44,20 +47,29 @@ fi
 if [ $WANTS_CONFIG -eq $TRUE ] ; then
 	mkdir -p ../logs > /dev/null 2>&1
 	./develop.py -t Release | tee $LOG
-	mkdir -p ./build-darwin-i386/newview/Release/Firestorm.app  # work around LL bug
+	# work around LL bug
+	mkdir -p ./build-darwin-i386/newview/Release/Firestorm.app
 fi
 
 if [ $WANTS_BUILD -eq $TRUE ] ; then
-	echo -n "Updating build version to "
-	buildVer=`grep -o -e "LL_VERSION_BUILD = [0-9]\+" llcommon/llversionviewer.h | cut -f 3 -d " "`
-	echo $((++buildVer))
-	sed -e "s#LL_VERSION_BUILD = [0-9][0-9]*#LL_VERSION_BUILD = ${buildVer}#" llcommon/llversionviewer.h > llcommon/llversionviewer.h1
-	mv llcommon/llversionviewer.h1 llcommon/llversionviewer.h
+	echo -n "Setting build version to "
+	buildVer=`hg summary | grep parent | sed 's/parent: //' | sed 's/:.*//'`
+	echo "$buildVer."
+	cp llcommon/llversionviewer.h llcommon/llversionviewer.h.build
+	trap "mv llcommon/llversionviewer.h.build llcommon/llversionviewer.h" INT TERM EXIT
+	sed -e "s#LL_VERSION_BUILD = [0-9][0-9]*#LL_VERSION_BUILD = ${buildVer}#"\
+		llcommon/llversionviewer.h.build > llcommon/llversionviewer.h
 	
 
-	echo "Building in progress... Check $LOG for verbose status"
+	echo "Building in progress. Check $LOG for verbose status."
 	# -sdk macosx10.6
-	xcodebuild -project build-darwin-i386/SecondLife.xcodeproj -alltargets -configuration Release GCC_VERSION=4.2 -sdk macosx10.6 GCC_OPTIMIZATION_LEVEL=3 ARCHS=i386 GCC_ENABLE_SSE3_EXTENSIONS=YES 2>&1 | tee $LOG | grep -e "[(make.*Error)|(xcodebuild.*Error)] "
-	echo "Complete"
+	xcodebuild -project build-darwin-i386/SecondLife.xcodeproj \
+		-alltargets -configuration Release GCC_VERSION=4.2 \
+		-sdk macosx10.6 GCC_OPTIMIZATION_LEVEL=3 ARCHS=i386 \
+		GCC_ENABLE_SSE3_EXTENSIONS=YES 2>&1 | tee $LOG | \
+		grep -e "[(make.*Error)|(xcodebuild.*Error)] "
+	trap - INT TERM EXIT
+	mv llcommon/llversionviewer.h.build llcommon/llversionviewer.h
+	echo "Complete."
 fi
-popd
+popd > /dev/null
