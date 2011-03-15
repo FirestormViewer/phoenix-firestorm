a1a9271554ee6fee7edc89056e7edc8fec398f2e
Techwolf Lupindo <none@none>
2011-03-14 23:12:09 -0400

    fix for STORM-1046, STORM-1014, STORM-1047 STORM-1046:[crashhunters] crash in LWorld::removeRegion STORM-1014: Viewer crash in LLSurface::getWaterHeight STORM-1047:[crashhunters] crash at LLViewerObjectList::renderObjectsForMap Early grab from viewer-development before merge with upstream

--- indra/newview/llviewerobjectlist.h
+++ indra/newview/llviewerobjectlist.h
@@ -87,6 +87,7 @@ public:
 
 	void shiftObjects(const LLVector3 &offset);
 
+	void clearAllMapObjectsInRegion(LLViewerRegion* regionp) ;
 	void renderObjectsForMap(LLNetMap &netmap);
 	void renderObjectBounds(const LLVector3 &center);
 
@@ -180,8 +181,7 @@ protected:
 
 	vobj_list_t mMapObjects;
 
-	typedef std::map<LLUUID, LLPointer<LLViewerObject> > vo_map;
-	vo_map mDeadObjects;	// Need to keep multiple entries per UUID
+	std::set<LLUUID> mDeadObjects;
 
 	std::map<LLUUID, LLPointer<LLViewerObject> > mUUIDObjectMap;
 
