2c80719c14130c159c1d5617ed4cc6ce934bf807
Arrehn <none@none>
2011-04-14 11:44:59 -0400

    Win32 compilation uses multiple CPUs again.

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -39,10 +39,10 @@ if (WINDOWS)
   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /Od /Zi /MD /MP"
       CACHE STRING "C++ compiler debug options" FORCE)
   set(CMAKE_CXX_FLAGS_RELWITHDEBINFO 
-      "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /Od /Zi /MD /Ob2 /Gm"
+      "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /Od /Zi /MD /MP /Ob2 /arch:SSE2"
       CACHE STRING "C++ compiler release-with-debug options" FORCE)
   set(CMAKE_CXX_FLAGS_RELEASE
-      "${CMAKE_CXX_FLAGS_RELEASE} ${LL_CXX_FLAGS} /O2 /Zi /MD /Ob2 /Oi /Ot /GF /Gy /arch:SSE2"
+      "${CMAKE_CXX_FLAGS_RELEASE} ${LL_CXX_FLAGS} /O2 /Zi /MD /MP /Ob2 /Oi /GF /Gy /arch:SSE2"
       CACHE STRING "C++ compiler release options" FORCE)
 	  
   if (LAA)
