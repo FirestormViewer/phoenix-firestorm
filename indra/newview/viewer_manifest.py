897fc2d9f1a533a746e82f0e9c3b949837c32b93
Arrehn <none@none>
2011-08-30 02:26:37 -0400

    Initial attempt to prepare libhunspell infrastructure

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -653,6 +653,7 @@ class DarwinManifest(ViewerManifest):
 
             # copy additional libs in <bundle>/Contents/MacOS/
             self.path("../packages/lib/release/libndofdev.dylib", dst="Resources/libndofdev.dylib")
+	    self.path("../packages/lib/release/libhunspell-1.3.dylib", dst="Resources/llibhunspell-1.3.dylib")
 
             self.path("../viewer_components/updater/scripts/darwin/update_install", "MacOS/update_install")
 
