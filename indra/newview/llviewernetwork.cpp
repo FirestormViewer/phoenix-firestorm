2cbcbb941ce6ba1fe85a8158a70f5467a441a79e
Mobius <mobiusryba@gmail.com>
2011-11-01 11:32:43 +0000

    V3 splash testing. Functionality works, still problems with CSS overriding :(

--- indra/newview/llviewernetwork.cpp
+++ indra/newview/llviewernetwork.cpp
@@ -159,7 +159,7 @@ private:
 // </AW opensim>
 
 
-const char* DEFAULT_LOGIN_PAGE = "http://phoenixviewer.com/app/loginFSbeta/";
+const char* DEFAULT_LOGIN_PAGE = "http://phoenixviewer.com/app/loginV3/";
 
 const char* SYSTEM_GRID_SLURL_BASE = "secondlife://%s/secondlife/";
 const char* MAIN_GRID_SLURL_BASE = "http://maps.secondlife.com/secondlife/";
