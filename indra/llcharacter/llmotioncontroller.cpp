39f1eab8054c9a91ac96ef220bcd9142eef861a5
Arrehn <none@none>
2012-07-08 11:10:05 -0400

    FIRE-6839, Backed out c91ab2b22a29, avatar animation speed issues.
    --HG--
    branch : Firestorm_4.1.1

--- indra/llcharacter/llmotioncontroller.cpp
+++ indra/llcharacter/llmotioncontroller.cpp
@@ -887,7 +887,10 @@ void LLMotionController::updateMotions(bool force_update)
 void LLMotionController::updateMotionsMinimal()
 {
 	// Always update mPrevTimerElapsed
-	mAnimTime = mPrevTimerElapsed = mTimer.getElapsedTimeF32();
+	//<FS:AO> backed out, causes face animation issues
+	//mAnimTime = mPrevTimerElapsed = mTimer.getElapsedTimeF32();
+        mPrevTimerElapsed = mTimer.getElapsedTimeF32();
+	//</FS:AO>
 
 	purgeExcessMotions();
 	updateLoadingMotions();
