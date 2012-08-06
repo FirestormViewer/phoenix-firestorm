916243844dadd640b57fbad9be3681b3bde1212c
Tank@test_master.ftrdhcpuser.net <Tank@test_master.ftrdhcpuser.net>
2012-08-06 15:20:48 -0700

    Remove physic stubs to make way for LL's PF version This will break mesh upload! A new stub will be reintroduced later to restore this

--- indra/cmake/ViewerMiscLibs.cmake
+++ indra/cmake/ViewerMiscLibs.cmake
@@ -3,7 +3,6 @@ include(Prebuilt)
 
 if (NOT STANDALONE)
   use_prebuilt_binary(libhunspell)
-  use_prebuilt_binary(nd_hacdConvexDecomposition)
   use_prebuilt_binary(libuuid)
   use_prebuilt_binary(slvoice)
   use_prebuilt_binary(fontconfig)
