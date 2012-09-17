ee7c476bf3405fc77078642e67d326469c013f9a
Tank_Master <none@none>
2012-09-17 00:56:43 -0700

    make autoreplace, aka autocorrect, work in chat again

--- indra/newview/llnearbychatbar.cpp
+++ indra/newview/llnearbychatbar.cpp
@@ -1,6 +1,4 @@
 /*
-#include "llautoreplace.h"
-	mChatBox->setAutoreplaceCallback(boost::bind(&LLAutoReplace::autoreplaceCallback, LLAutoReplace::getInstance(), _1, _2));
 
 This file is not used by Firestorm. It's being kept in place to track merges.
 
