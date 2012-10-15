8ee8001d775dc075a9e72998aade9e4aa4786290
Ansariel <none@none>
2012-10-15 23:04:17 +0200

    Calculate proper spillover item width for menus

--- indra/llui/llmenugl.cpp
+++ indra/llui/llmenugl.cpp
@@ -1965,7 +1965,10 @@ void LLMenuGL::arrange( void )
 		}
 
 		// *FIX: create the item first and then ask for its dimensions?
-		S32 spillover_item_width = PLAIN_PAD_PIXELS + LLFontGL::getFontSansSerif()->getWidth( std::string("More") ); // *TODO: Translate
+		// <FS:Ansariel> Calculate proper width for localized string
+		//S32 spillover_item_width = PLAIN_PAD_PIXELS + LLFontGL::getFontSansSerif()->getWidth( std::string("More") ); // *TODO: Translate
+		S32 spillover_item_width = PLAIN_PAD_PIXELS + LLFontGL::getFontSansSerif()->getWidth( LLTrans::getString("More") );
+		// </FS:Ansariel>
 		S32 spillover_item_height = LLFontGL::getFontSansSerif()->getLineHeight() + MENU_ITEM_PADDING;
 
 		// Scrolling support
