0f110e84f4fce57102bb932594b6f96ea05d36d9
Ansariel <none@none>
2012-10-26 11:10:44 +0200

    FIRE-7773 / MAINT-1686: Fixed another occurance of falsely set parcel category

--- indra/newview/llfloaterland.cpp
+++ indra/newview/llfloaterland.cpp
@@ -2232,8 +2232,12 @@ void LLPanelLandOptions::refreshSearch()
 		mCheckShowDirectory->setEnabled(FALSE);
 
 		// *TODO:Translate
-		const std::string& none_string = LLParcel::getCategoryUIString(LLParcel::C_NONE);
-		mCategoryCombo->setSimple(none_string);
+		// <FS:Ansariel> FIRE-7773: Parcel categories don't stay selected
+		//const std::string& none_string = LLParcel::getCategoryUIString(LLParcel::C_NONE);
+		//mCategoryCombo->setSimple(none_string);
+		const std::string& none_string = LLParcel::getCategoryString(LLParcel::C_ANY);
+		mCategoryCombo->setValue(none_string);
+		// </FS:Ansariel>
 		mCategoryCombo->setEnabled(FALSE);
 		return;
 	}
