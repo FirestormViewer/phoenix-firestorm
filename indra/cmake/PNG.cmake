ada4a74cc46e2b26ba4f9e190cbab30a0cd15b71
Nicky <none@none>
2012-10-31 20:17:55 +0100

    CMP0011 is not known on older cmake versions and leads to build failures there.

--- indra/cmake/PNG.cmake
+++ indra/cmake/PNG.cmake
@@ -1,9 +1,13 @@
 # -*- cmake -*-
 
 # Silence warning about overriding findZLIB.cmake with our own copy.
-cmake_policy(SET CMP0017 OLD)
+
+if(${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 8 AND ${CMAKE_PATCH_VERSION} GREATER 3) 
+ cmake_policy(SET CMP0017 OLD)
+ cmake_policy(SET CMP0011 NEW)
+endif(${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 8 AND ${CMAKE_PATCH_VERSION} GREATER 3) 
+
 # Silence warning about using policies to silence warnings about using policies.
-cmake_policy(SET CMP0011 NEW)
 
 include(Prebuilt)
 
