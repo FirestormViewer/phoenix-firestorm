b72820936539c83bb1bd7e927c98f3b8b82a5193
Ansariel <none@none>
2012-11-25 22:27:06 +0100

    FIRE-4140: Fix for group invite button on profile sometimes doesn't work

--- indra/newview/llpanelgroupinvite.cpp
+++ indra/newview/llpanelgroupinvite.cpp
@@ -453,7 +453,9 @@ void LLPanelGroupInvite::addUsers(uuid_vec_t& agent_ids)
 			//looks like user try to invite offline friend
 			//for offline avatar_id gObjectList.findObject() will return null
 			//so we need to do this additional search in avatar tracker, see EXT-4732
-			if (LLAvatarTracker::instance().isBuddy(agent_id))
+			// <FS:Ansariel> FIRE-4140: Group invite button on profile sometimes doesn't work
+			//if (LLAvatarTracker::instance().isBuddy(agent_id))
+			// </FS:Ansariel>
 			{
 				if (!gCacheName->getFullName(agent_id, fullname))
 				{
