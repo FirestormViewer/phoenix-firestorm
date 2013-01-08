123facf7b7bddd932545a1f27e36fc4bd16c0892
Ansariel <none@none>
2013-01-08 17:51:59 +0100

    Fixed license header

--- indra/llcommon/ndcallstack.h
+++ indra/llcommon/ndcallstack.h
@@ -20,9 +20,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * The Phoenix Viewer Project, Inc., 1831 Oakwood Drive, Fairmont, Minnesota 56031-3225 USA
- * http://www.phoenixviewer.com
- * $/LicenseInfo
+ * The Phoenix Firestorm Project, Inc., 1831 Oakwood Drive, Fairmont, Minnesota 56031-3225 USA
+ * http://www.firestormviewer.org
+ * $/LicenseInfo$
  */
 
 #include "ndstackwalk.h"
