fc62a10d0d7304c4fe99de100919433e5eee60be
Ansariel <none@none>
2013-03-02 00:37:18 +0100

    Cleaned up fsfloaterim.cpp after SSB/RLVa merge

--- indra/newview/fsfloaterim.cpp
+++ indra/newview/fsfloaterim.cpp
@@ -355,56 +355,6 @@ void FSFloaterIM::sendMsg()
 			}
 // [/RLVa:KB]
 
-// [RLVa:KB] - Checked: 2010-11-30 (RLVa-1.3.0c) | Modified: RLVa-1.3.0c
-			if ( (gRlvHandler.hasBehaviour(RLV_BHVR_SENDIM)) || (gRlvHandler.hasBehaviour(RLV_BHVR_SENDIMTO)) )
-			{
-				LLIMModel::LLIMSession* pIMSession = LLIMModel::instance().findIMSession(mSessionID);
-				RLV_ASSERT(pIMSession);
-
-				bool fRlvFilter = !pIMSession;
-				if (pIMSession)
-				{
-					switch (pIMSession->mSessionType)
-					{
-						case LLIMModel::LLIMSession::P2P_SESSION:	// One-on-one IM
-							fRlvFilter = !gRlvHandler.canSendIM(mOtherParticipantUUID);
-							break;
-						case LLIMModel::LLIMSession::GROUP_SESSION:	// Group chat
-							fRlvFilter = !gRlvHandler.canSendIM(mSessionID);
-							break;
-						case LLIMModel::LLIMSession::ADHOC_SESSION:	// Conference chat: allow if all participants can be sent an IM
-							{
-								if (!pIMSession->mSpeakers)
-								{
-									fRlvFilter = true;
-									break;
-								}
-
-								LLSpeakerMgr::speaker_list_t speakers;
-								pIMSession->mSpeakers->getSpeakerList(&speakers, TRUE);
-								for (LLSpeakerMgr::speaker_list_t::const_iterator itSpeaker = speakers.begin(); 
-										itSpeaker != speakers.end(); ++itSpeaker)
-								{
-									const LLSpeaker* pSpeaker = *itSpeaker;
-									if ( (gAgent.getID() != pSpeaker->mID) && (!gRlvHandler.canSendIM(pSpeaker->mID)) )
-									{
-										fRlvFilter = true;
-										break;
-									}
-								}
-							}
-							break;
-						default:
-							fRlvFilter = true;
-							break;
-					}
-				}
-
-				if (fRlvFilter)
-					utf8_text = RlvStrings::getString(RLV_STRING_BLOCKED_SENDIM);
-			}
-// [/RLVa:KB]
-
 			if (mSessionInitialized)
 			{
 				LLIMModel::sendMessage(utf8_text, mSessionID,
