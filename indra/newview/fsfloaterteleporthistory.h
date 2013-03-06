7abcf40dec2be03edbad7ebf247823d45452b7c7
Ansariel <none@none>
2013-03-06 14:07:56 +0100

    Fixed some Firestorm license headers

--- indra/newview/fsfloaterteleporthistory.h
+++ indra/newview/fsfloaterteleporthistory.h
@@ -19,6 +19,10 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * The Phoenix Firestorm Project, Inc., 1831 Oakwood Drive, Fairmont, Minnesota 56031-3225 USA
+ * http://www.firestormviewer.org
+ * $/LicenseInfo$
  */
 
 #ifndef FS_FLOATERTELEPORTHISTORY_H
