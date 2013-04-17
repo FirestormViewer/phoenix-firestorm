723f415ad3585f3fdf675859b73480071224574d
Kitty Barnett <develop@catznip.com>
2013-04-17 21:13:14 +0200

    - internal : indicate failure when receiving @adjustheight if using server bakes
    --HG--
    branch : RLVa

--- indra/newview/rlvhandler.cpp
+++ indra/newview/rlvhandler.cpp
@@ -1611,7 +1611,10 @@ ERlvCmdRet RlvHandler::processForceCommand(const RlvCommand& rlvCmd) const
 				{
 					F32 nValue = (rlvCmdOption.m_nPelvisToFoot - gAgentAvatarp->getPelvisToFoot()) * rlvCmdOption.m_nPelvisToFootDeltaMult;
 					nValue += rlvCmdOption.m_nPelvisToFootOffset;
-					gSavedSettings.setF32(RLV_SETTING_AVATAROFFSET_Z, llclamp<F32>(nValue, -1.0f, 1.0f));
+					if (!gAgentAvatarp->isUsingServerBakes())
+						gSavedSettings.setF32(RLV_SETTING_AVATAROFFSET_Z, llclamp<F32>(nValue, -1.0f, 1.0f));
+					else
+						eRet = RLV_RET_FAILED_DISABLED;
 				}
 			}
 			break;
