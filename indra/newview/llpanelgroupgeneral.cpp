2562add8f9f4a7bbe76e1082b4fdc0ae2a004266
Kitty Barnett <develop@catznip.com>
2013-05-01 22:15:31 +0200

    FIRE-8989: Duplicate names in group window
    --HG--
    branch : Firestorm_4.4.1

--- indra/newview/llpanelgroupgeneral.cpp
+++ indra/newview/llpanelgroupgeneral.cpp
@@ -788,17 +788,17 @@ void LLPanelGroupGeneral::updateMembers()
 			continue;
 		}
 
-		if (LLAvatarNameCache::get(mMemberProgress->first, &av_name))
+//		if (LLAvatarNameCache::get(mMemberProgress->first, &av_name))
 		{
 			addMember(mMemberProgress->second);
 		}
-		else
-		{
-			// If name is not cached, onNameCache() should be called when it is cached and add this member to list.
-			LLAvatarNameCache::get(mMemberProgress->first, 
-									boost::bind(&LLPanelGroupGeneral::onNameCache,
-												this, gdatap->getMemberVersion(), member, _2));
-		}
+//		else
+//		{
+//			// If name is not cached, onNameCache() should be called when it is cached and add this member to list.
+//			LLAvatarNameCache::get(mMemberProgress->first, 
+//									boost::bind(&LLPanelGroupGeneral::onNameCache,
+//												this, gdatap->getMemberVersion(), member, _2));
+//		}
 	}
 
 	if (mMemberProgress == gdatap->mMembers.end())
