3d5b33998e0e0d094a4e46a5d4aa90181395e62c
Kitty Barnett <develop@catznip.com>
2013-06-06 21:44:52 +0200

    - internal : deprecate the legacy avatar Z-offset unless manually set through RLVa (or the debug setting floater)     -> setting will also no longer persist across sessions
    --HG--
    branch : RLVa

--- indra/newview/llvoavatarself.cpp
+++ indra/newview/llvoavatarself.cpp
@@ -3209,6 +3209,6 @@ void LLVOAvatarSelf::dumpWearableInfo(LLAPRFile& outfile)
 // [RLVa:KB] - Checked: 2013-03-03 (RLVa-1.4.8)
 F32 LLVOAvatarSelf::getAvatarOffset() /*const*/
 {
-	return (isUsingServerBakes()) ? LLAvatarAppearance::getAvatarOffset() : RlvSettings::getAvatarOffsetZ();
+	return ( (isUsingServerBakes()) || (0.0f == RlvSettings::getAvatarOffsetZ()) ) ? LLAvatarAppearance::getAvatarOffset() : RlvSettings::getAvatarOffsetZ();
 }
 // [/RLVa:KB]
