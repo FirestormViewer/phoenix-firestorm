94327cee99431eefad2c8b1b200e7494970c63dc
Cinders <cinder.roxley@phoenixviewer.com>
2013-06-25 22:44:34 -0600

    Remove some unused GridInfo entries

--- indra/newview/fsgridhandler.h
+++ indra/newview/fsgridhandler.h
@@ -59,9 +59,6 @@ extern const char* DEFAULT_LOGIN_PAGE;
 #define GRID_PROFILE_URI_VALUE "profileuri"
 #define GRID_SENDGRIDINFO "SendGridInfoToViewerOnLogin"
 #define GRID_DIRECTORY_FEE "DirectoryFee"
-#define GRID_CURRENCY_SYMBOL "CurrencySymbol"
-#define GRID_REAL_CURRENCY_SYMBOL "RealCurrencySymbol"
-#define GRID_MAXGROUPS "MaxGroups"
 #define GRID_PLATFORM "platform"
 #define GRID_MESSAGE "message"
 // </FS:CR> Aurora Sim
