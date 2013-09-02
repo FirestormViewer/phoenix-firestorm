dd07882b67dcbcbb471027153cb08b5b9a9e9e28
Cinders <cinder.roxley@phoenixviewer.com>
2013-09-02 12:54:01 -0600

    Guard if the legacy first name is empty.

--- indra/llcommon/llavatarname.cpp
+++ indra/llcommon/llavatarname.cpp
@@ -170,7 +170,7 @@ std::string LLAvatarName::getCompleteName() const
 			// If this particular display name is defaulted (i.e. based on user name),
 			// then display only the easier to read instance of the person's name.
 			// <FS:CR> FIRE-6659 - Trim "Resident" from legacy name
-			if (sTrimResidentSurname)
+			if (sTrimResidentSurname || mLegacyFirstName.empty())
 				name = mDisplayName;
 			else if (mLegacyLastName.empty())
 				name = mLegacyFirstName + " Resident";
