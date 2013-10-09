5d4c672aaf76d657485b3707a45c7a9b19fd754a
Nicky <none@none>
2013-10-09 19:01:31 +0200

    Must use ll_aligned_alloc to work in all configurations.

--- indra/llcommon/nd/ndobjectpool.h
+++ indra/llcommon/nd/ndobjectpool.h
@@ -45,7 +45,7 @@ namespace nd
 
 			void grow()
 			{
-				char *pMemory = reinterpret_cast< char* >( nd::allocators::ndMalloc( mObjectSize * AllocationSize, Alignment ) );
+				char *pMemory = reinterpret_cast< char* >( ll_aligned_malloc( mObjectSize * AllocationSize, Alignment ) );
 				ObjectMemory *pPrev = &mMemory;
 
 				for( int i = 0; i < AllocationSize; ++i )
