c0531d545066aea542562736bded13a2035768ee
Cinders <cinder.roxley@phoenixviewer.com>
2013-11-15 06:32:53 -0700

    Put my code back

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -531,17 +531,18 @@ void idle_afk_check()
 	F32 current_idle = gAwayTriggerTimer.getElapsedTimeF32();
 //	F32 afk_timeout  = gSavedSettings.getS32("AFKTimeout");
 // [RLVa:KB] - Checked: 2010-05-03 (RLVa-1.2.0g) | Modified: RLVa-1.2.0g
-	// <FS:CR> Cache frequently hit location
-	static LLCachedControl<S32> sAFKTimeout(gSavedSettings, "AFKTimeout");
-	S32 afk_timeout = sAFKTimeout;
 #ifdef RLV_EXTENSION_CMD_ALLOWIDLE
 	// Enforce an idle time of 30 minutes if @allowidle=n restricted
-	afk_timeout = (!gRlvHandler.hasBehaviour(RLV_BHVR_ALLOWIDLE)) ? afk_timeout : 60 * 30;
+	// <FS:CR> Cache frequently hit location
+	static LLCachedControl<S32> sAFKTimeout(gSavedSettings, "AFKTimeout");
+	S32 afk_timeout = (!gRlvHandler.hasBehaviour(RLV_BHVR_ALLOWIDLE)) ? sAFKTimeout : 60 * 30;
+#else
+	static LLCachedControl<S32> afk_timeout(gSavedSettings, "AFKTimeout");	// <FS:CR>
 #endif // RLV_EXTENSION_CMD_ALLOWIDLE
 // [/RLVa:KB]
 	// <FS:CR> Explicit conversions just cos.
 	//if (afk_timeout && (current_idle > afk_timeout) && ! gAgent.getAFK())
-	if (afk_timeout && (current_idle > static_cast<F32>(afk_timeout)) && ! gAgent.getAFK())
+	if (afk_timeout() && (current_idle > static_cast<F32>(afk_timeout)) && ! gAgent.getAFK())
 	{
 		LL_INFOS("IdleAway") << "Idle more than " << afk_timeout << " seconds: automatically changing to Away status" << LL_ENDL;
 		gAgent.setAFK();
