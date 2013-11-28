bee4a7f0d8f6197423ea47f8405ee7b0121febd9
Nicky <none@none>
2013-11-28 13:46:23 +0100

    Don't try to install 3p-leapmotion when leap support is disabled.

--- indra/cmake/LeapMotion.cmake
+++ indra/cmake/LeapMotion.cmake
@@ -6,19 +6,21 @@ if (INSTALL_PROPRIETARY)
   set(LEAPMOTION ON CACHE BOOL "Building with Leap Motion Controller support.")
 endif (INSTALL_PROPRIETARY)
 
-if (STANDALONE)
-  # *TODO: Standalone support
-else (STANDALONE)
-  include(Prebuilt)
-  use_prebuilt_binary(leap-motion)
-if (DARWIN)
+if( LEAPMOTION )
+  if (STANDALONE)
+    # *TODO: Standalone support
+  else (STANDALONE)
+    include(Prebuilt)
+    use_prebuilt_binary(leap-motion)
+  if (DARWIN)
     set(LEAP_MOTION_LIBRARY libLeap.dylib)
- elseif (WINDOWS)
-   set(LEAP_MOTION_LIBRARY
-	    debug Leapd.dll
-		optimized Leap.dll)
- elseif (LINUX)
-   set(LEAP_MOTION_LIBRARY libLeap.so)
- endif()
-  set(LEAP_MOTION_INCLUDE_DIR ${LIBS_OPEN_DIR}/leap-motion)
-endif (STANDALONE)
+  elseif (WINDOWS)
+    set(LEAP_MOTION_LIBRARY
+        debug Leapd.dll
+        optimized Leap.dll)
+   elseif (LINUX)
+     set(LEAP_MOTION_LIBRARY libLeap.so)
+   endif()
+    set(LEAP_MOTION_INCLUDE_DIR ${LIBS_OPEN_DIR}/leap-motion)
+  endif (STANDALONE)
+endif( LEAPMOTION )
