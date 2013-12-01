c18f33a1bad82f51e7d55238874edf262fccba23
Ansariel <none@none>
2013-12-01 13:30:59 +0100

    Re-add particle rendering optimization that got lost in merge orgy upstream

--- indra/newview/lldrawpoolalpha.cpp
+++ indra/newview/lldrawpoolalpha.cpp
@@ -605,7 +605,11 @@ void LLDrawPoolAlpha::renderAlpha(U32 mask, S32 pass)
 				// If this alpha mesh has glow, then draw it a second time to add the destination-alpha (=glow).  Interleaving these state-changing calls could be expensive, but glow must be drawn Z-sorted with alpha.
 				if (current_shader && 
 					draw_glow_for_this_partition &&
-					params.mVertexBuffer->hasDataType(LLVertexBuffer::TYPE_EMISSIVE))
+					// <FS:Ansariel> Re-add particle rendering optimization
+					//params.mVertexBuffer->hasDataType(LLVertexBuffer::TYPE_EMISSIVE))
+					params.mVertexBuffer->hasDataType(LLVertexBuffer::TYPE_EMISSIVE) &&
+					(!params.mParticle || params.mHasGlow))
+					// </FS:Ansariel>
 				{
 					// install glow-accumulating blend mode
 					gGL.blendFunc(LLRender::BF_ZERO, LLRender::BF_ONE, // don't touch color
