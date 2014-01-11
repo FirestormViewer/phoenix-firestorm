a3dc7915f4ce2562e4892740b3a999798080ca9e
Ansariel <none@none>
2014-01-11 19:59:07 +0100

    And make it even more obvious...

--- indra/newview/llappviewerwin32.cpp
+++ indra/newview/llappviewerwin32.cpp
@@ -597,11 +597,11 @@ bool LLAppViewerWin32::initHardwareTest()
 	if (gGLManager.mVRAM == 0)
 	{
 		// <FS:Ansariel> FIRE-12671: Force VRAM if DirectX detection is broken
-		S32 forced_memory;
-		if ((forced_memory = gSavedSettings.getS32("FSForcedMemory")) > 0)
+		S32 forced_video_memory;
+		if ((forced_video_memory = gSavedSettings.getS32("FSForcedVideoMemory")) > 0)
 		{
-			LL_INFOS("AppInit") << "Forcing VRAM to " << forced_memory << " MB" << LL_ENDL;
-			gGLManager.mVRAM = forced_memory;
+			LL_INFOS("AppInit") << "Forcing VRAM to " << forced_video_memory << " MB" << LL_ENDL;
+			gGLManager.mVRAM = forced_video_memory;
 		}
 		else
 		// </FS:Ansariel>
