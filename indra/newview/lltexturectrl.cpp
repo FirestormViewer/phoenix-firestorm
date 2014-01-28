876f95295a80c6e7b039c0d41ab95f1b8fb167fc
Ansariel <none@none>
2014-01-28 12:28:30 +0100

    FIRE-10125: Texture picker allows dragging of textures while in preview mode

--- indra/newview/lltexturectrl.cpp
+++ indra/newview/lltexturectrl.cpp
@@ -1479,7 +1479,10 @@ BOOL LLTextureCtrl::handleDragAndDrop(S32 x, S32 y, MASK mask,
 	LLInventoryItem* item = (LLInventoryItem*)cargo_data; 
 	bool is_mesh = cargo_type == DAD_MESH;
 
-	if (getEnabled() &&
+	// <FS:Ansariel> FIRE-10125: Texture picker allows dragging of textures while in preview mode
+	//if (getEnabled() &&
+	if (getEnabled() && !mPreviewMode &&
+	// </FS:Ansariel>
 		((cargo_type == DAD_TEXTURE) || is_mesh) &&
 		 allowDrop(item))
 	{
