e91b232f11e7bbe281bf800e41e2df28cdb8f6cd
Ansariel <none@none>
2014-01-28 12:13:24 +0100

    FIRE-10125: Textures can be dragged into sculpt texture picker for meshes

--- indra/newview/llpanelobject.h
+++ indra/newview/llpanelobject.h
@@ -88,6 +88,8 @@ public:
 	void     		onSelectSculpt(const LLSD& data);
 	BOOL     		onDropSculpt(LLInventoryItem* item);
 	static void     onCommitSculptType(    LLUICtrl *ctrl, void* userdata);
+	// <FS:Ansariel> FIRE-10125: Textures can be dragged into sculpt texture picker for meshes
+	BOOL			onDragSculpt();
 
 protected:
 	void			getState();
@@ -225,6 +227,11 @@ protected:
 
 	LLPointer<LLViewerObject> mObject;
 	LLPointer<LLViewerObject> mRootObject;
+
+	// <FS:Ansariel> FIRE-10125: Textures can be dragged into sculpt texture picker for meshes
+	bool			mIsMesh;
+	bool			mIsEditable;
+	// </FS:Ansariel>
 };
 
 #endif
