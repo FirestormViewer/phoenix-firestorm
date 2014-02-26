e4d224d390370838f7ad1782f6d41594c711b498
Kitty Barnett <develop@catznip.com>
2014-02-26 22:53:41 +0100

    Incremented API version number to 2.8.0
    --HG--
    branch : RLVa

--- indra/newview/rlvdefines.h
+++ indra/newview/rlvdefines.h
@@ -57,7 +57,7 @@
 
 // Version of the specifcation we support
 const S32 RLV_VERSION_MAJOR = 2;
-const S32 RLV_VERSION_MINOR = 7;
+const S32 RLV_VERSION_MINOR = 8;
 const S32 RLV_VERSION_PATCH = 0;
 const S32 RLV_VERSION_BUILD = 0;
 
