daa6e0b181087711cb00ba24325d40e19ac5640d
Ansariel <none@none>
2014-02-27 13:39:42 +0100

    Fix another protected folder check error

--- indra/newview/llinventoryfunctions.cpp
+++ indra/newview/llinventoryfunctions.cpp
@@ -469,16 +469,18 @@ BOOL get_is_category_renameable(const LLInventoryModel* model, const LLUUID& id)
 	}
 // [/RLVa:KB]
 
-	// ## Zi: Animation Overrider
-	if(((id==AOEngine::instance().getAOFolder() || model->isObjectDescendentOf(id,AOEngine::instance().getAOFolder()))
-		&& gSavedPerAccountSettings.getBOOL("ProtectAOFolders"))
-//-TT Client LSL Bridge
-		|| (id==FSLSLBridge::instance().getBridgeFolder() || (model->isObjectDescendentOf(id,FSLSLBridge::instance().getBridgeFolder())
-			&& gSavedPerAccountSettings.getBOOL("ProtectBridgeFolder")))
+	// <FS> Protected Folders
+	if (
+		((id == AOEngine::instance().getAOFolder() || model->isObjectDescendentOf(id, AOEngine::instance().getAOFolder()))
+			&& gSavedPerAccountSettings.getBOOL("ProtectAOFolders"))
+		||
+		((id == FSLSLBridge::instance().getBridgeFolder() || model->isObjectDescendentOf(id, FSLSLBridge::instance().getBridgeFolder()))
+			&& gSavedPerAccountSettings.getBOOL("ProtectBridgeFolder"))
 		)
-//-TT
+	{
 		return FALSE;
-	// ## Zi: Animation Overrider
+	}
+	// </FS> Protected Folders
 
 	LLViewerInventoryCategory* cat = model->getCategory(id);
 
