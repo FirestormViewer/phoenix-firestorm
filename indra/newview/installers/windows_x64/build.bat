8c2c0a05c90c46448bd4f9a8b75c55d2184ea794
Nicky <none@none>
2014-03-06 13:02:23 +0100

    FIRE-13025; Ignore the embeeded version info in SLVoice.exe during install, instead make everything dependent on firestorm-bin.exe. - Package Leap.dll conditionally.

--- indra/newview/installers/windows_x64/build.bat
+++ indra/newview/installers/windows_x64/build.bat
@@ -21,6 +21,14 @@ set HGCHANGE=%9
 
 set PATH=%PATH%;%1\..\..\packages\bin\wix
 
+if exist %VIEWER_BUILDDIR%\Leap.dll (
+  set PACKAGE_LEAP=PACKAGE_LEAP=1
+  echo Packaging Leap.dll
+) else (
+  set PACKAGE_LEAP=PACKAGE_LEAP=0
+  echo Not Packaging Leap.dll
+)
+
 heat dir %VIEWER_BUILDDIR%\character -gg -cg fs_character -var var.BUILDDIR -dr INSTALLDIR -out character.wxs
 heat dir %VIEWER_BUILDDIR%\fonts -gg -cg fs_fonts -var var.BUILDDIR -dr INSTALLDIR -out fonts.wxs
 heat dir %VIEWER_BUILDDIR%\fs_resources -gg -cg fs_fsres -var var.BUILDDIR -dr INSTALLDIR -out fs_resources.wxs
@@ -33,7 +41,7 @@ candle -dBUILDDIR=%VIEWER_BUILDDIR%\fonts fonts.wxs
 candle -dBUILDDIR=%VIEWER_BUILDDIR%\fs_resources fs_resources.wxs
 
 candle -dPLUGIN_SOURCEDIR=%PLUGIN_SOURCEDIR% %~dp0\llplugin.wxs
-candle -dPROGRAM_FILE=%PROGRAM_FILE% -dMAJOR=%MAJOR% -dMINOR=%MINOR% -dHGCHANGE=%HGCHANGE% -dBUILDDIR=%VIEWER_BUILDDIR%\ -dWIX_SOURCEDIR=%~dp0 %~dp0\firestorm.wxs
+candle -dPROGRAM_FILE=%PROGRAM_FILE% -dMAJOR=%MAJOR% -dMINOR=%MINOR% -dHGCHANGE=%HGCHANGE% -dBUILDDIR=%VIEWER_BUILDDIR%\ -dWIX_SOURCEDIR=%~dp0 -d%PACKAGE_LEAP% %~dp0\firestorm.wxs
 candle -dPROGRAM_FILE=%PROGRAM_FILE% -dPROGRAM_VERSION=%PROGRAM_VERSION% -dCHANNEL_NAME=%CHANNEL_NAME% -dSETTINGS_FILE=%SETTINGS_FILE% -dPROGRAM_NAME=%PROGRAM_NAME% -dBUILDDIR=%VIEWER_BUILDDIR%\ %~dp0\registry.wxs
 
 light -sval -ext WixUIExtension -cultures:en-us -out %VIEWER_BUILDDIR%\%OUTPUT_FILE%.msi firestorm.wixobj character.wixobj fonts.wixobj fs_resources.wixobj llplugin.wixobj registry.wixobj
