7889dbc9e26da41a4fec76ef24677b1928e3a013
Techwolf Lupindo <none@none>
2014-04-06 22:23:07 -0400

    Don't display 32-bit compatibility messages for 64-bit builds.

--- indra/newview/linux_tools/wrapper.sh
+++ indra/newview/linux_tools/wrapper.sh
@@ -164,7 +164,7 @@ LL_RUN_ERR=$?
 if [ $LL_RUN_ERR -ne 0 ]; then
 	# generic error running the binary
 	echo '*** Bad shutdown ($LL_RUN_ERR). ***'
-	if [ "$(uname -m)" = "x86_64" ]; then
+	if [ "$(uname -m)" = "x86_64" ] && [ "$(file bin/do-not-directly-run-firestorm-bin | grep -o 64-bit )" != "64-bit" ]; then
 		echo
 		cat << EOFMARKER
 You are running the Firestorm Viewer on a x86_64 platform.  The
