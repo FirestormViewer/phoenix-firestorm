d09fbe7498e5868152c9b4e400ca985f88eebe74
Ansariel <none@none>
2014-06-10 22:22:23 +0200

    Re-apply fix for FIRE-3925

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -3949,7 +3949,11 @@ LLSD LLAppViewer::getViewerInfo() const
 	{
 		LLTrace::Recording& last_frame = LLTrace::get_frame_recording().getLastRecording();
 		info["PACKETS_LOST"] = last_frame.getSum(LLStatViewer::PACKETS_LOST);
-		info["PACKETS_IN"] = last_frame.getSum(LLStatViewer::PACKETS_IN);
+		// <FS:Ansariel> FIRE-3925: Formatting fails when reaching 1,000,000;
+		//               Use S32 since it will be used in LLStringUtil::formatNumber anyway.
+		//info["PACKETS_IN"] = last_frame.getSum(LLStatViewer::PACKETS_IN);
+		info["PACKETS_IN"] = S32(last_frame.getSum(LLStatViewer::PACKETS_IN));
+		// </FS:Ansariel>
 		info["PACKETS_PCT"] = 100.f*info["PACKETS_LOST"].asReal() / info["PACKETS_IN"].asReal();
 	}
 
