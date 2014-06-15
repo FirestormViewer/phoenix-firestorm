cd4fd4f68be742cda2e14db0e04b843956d84449
Northspring <pantera.polnocy@phoenixviewer.com>
2014-06-16 01:35:11 +0200

    Flight Assistant code rewrite + some minor script modifications

--- indra/newview/fslslbridge.cpp
+++ indra/newview/fslslbridge.cpp
@@ -67,10 +67,10 @@
 const std::string FS_BRIDGE_FOLDER = "#LSL Bridge";
 const std::string FS_BRIDGE_CONTAINER_FOLDER = "Landscaping";
 const U32 FS_BRIDGE_MAJOR_VERSION = 2;
-const U32 FS_BRIDGE_MINOR_VERSION = 11;
+const U32 FS_BRIDGE_MINOR_VERSION = 12;
 const U32 FS_MAX_MINOR_VERSION = 99;
 
-//current script version is 2.11
+//current script version is 2.12
 const std::string UPLOAD_SCRIPT_CURRENT = "EBEDD1D2-A320-43f5-88CF-DD47BBCA5DFB.lsltxt";
 
 //
