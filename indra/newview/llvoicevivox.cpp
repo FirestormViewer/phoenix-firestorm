5782229106964fd57d33b11c8eef802bed4ed114
Aura Linden <aura@lindenlab.com>
2014-08-15 17:30:09 -0700

    Added delay to Windows shutdown to let SLVoice die a good death.
    --HG--
    branch : Firestorm_4.6.9

--- indra/newview/llvoicevivox.cpp
+++ indra/newview/llvoicevivox.cpp
@@ -805,12 +805,9 @@ void LLVivoxVoiceClient::stateMachine()
 						params.args.add("-ll");
 						params.args.add(loglevel);
 
-						//if (loglevel != "0") //SPATTERS
-						{
-							std::string log_folder = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, "");
-							params.args.add("-lf");
-							params.args.add(log_folder);
-						}
+						std::string log_folder = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, "");
+						params.args.add("-lf");
+						params.args.add(log_folder);
 
 						if(!shutdown_timeout.empty())
 						{
@@ -1567,7 +1564,6 @@ void LLVivoxVoiceClient::stateMachine()
 		//MARK: stateLoggingOut
 		case stateLoggingOut:			// waiting for logout response
 			// The handler for the AccountLoginStateChangeEvent will transition from here to stateLoggedOut.
-			LL_INFOS() << "SPATTERS do I need to stay alive here?" << LL_ENDL;
 		break;
 		
 		//MARK: stateLoggedOut
@@ -1671,6 +1667,9 @@ void LLVivoxVoiceClient::stateMachine()
 
 void LLVivoxVoiceClient::closeSocket(void)
 {
+#ifdef LL_WINDOWS
+	_sleep(3000);	//Wait a moment for socket to close.  SPATTERS
+#endif  
 	mSocket.reset();
 	mConnected = false;
 	mConnectorHandle.clear();
