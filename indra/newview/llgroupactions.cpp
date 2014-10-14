dee88bbdf11a778ac548302104cdce97baae0d9a
Tank_Master <none@none>
2014-10-13 22:46:05 -0700

    group banz

--- indra/newview/llgroupactions.cpp
+++ indra/newview/llgroupactions.cpp
@@ -173,7 +173,7 @@ public:
 
 	void changed(LLGroupChange gc)
 	{
-		if (gc == GC_MEMBER_DATA && !mRequestProcessed)
+		if (gc == GC_PROPERTIES && !mRequestProcessed)
 		{
 			LLGroupMgrGroupData* gdatap = LLGroupMgr::getInstance()->getGroupData(mGroupId);
 			if (!gdatap)
@@ -183,9 +183,6 @@ public:
 			else if (!gdatap->isMemberDataComplete())
 			{
 				LL_WARNS() << "LLGroupMgr::getInstance()->getGroupData()->isMemberDataComplete() was FALSE" << LL_ENDL;
-			}
-			else
-			{
 				processGroupData();
 				mRequestProcessed = true;
 			}
