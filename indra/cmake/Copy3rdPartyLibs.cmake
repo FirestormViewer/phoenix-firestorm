4c9c6deda5fdbaba389ee9f8a280ef419c311365
Nicky <none@none>
2014-10-26 22:50:55 +0100

    libllqtwebkit.dylib does not exist anymore.

--- indra/cmake/Copy3rdPartyLibs.cmake
+++ indra/cmake/Copy3rdPartyLibs.cmake
@@ -251,13 +251,6 @@ elseif(DARWIN)
         libgrowl++.dylib
        )
 
-    # <FS:ND> libllqtwebkit.dylib comes from a prebuild slplugin. It needs to be fetched from somewhere else
-	# when building x64/universal. Probably it's best to copy it in fs_viewer_manifest.py for those cases.
-    if( NOT ND_BUILD64BIT_ARCH )
-      set(release_files ${release_files} libllqtwebkit.dylib )
-    endif( NOT ND_BUILD64BIT_ARCH )
-	# </FS:ND>
-
     # <FS:ND> We only ever need google breakpad when crash reporting is used
     if(RELEASE_CRASH_REPORTING OR NON_RELEASE_CRASH_REPORTING)
       set(release_files ${release_files} "libexception_handler.dylib")
