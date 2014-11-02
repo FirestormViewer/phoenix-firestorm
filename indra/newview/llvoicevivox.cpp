217f337a866e1353ce4a78191f13071395117a4d
Ansariel <none@none>
2014-11-02 20:17:26 +0100

    FIRE-14864: Shot in the blue fix for APR mess with paths containing spaces and trailing directory delimiters

--- indra/newview/llvoicevivox.cpp
+++ indra/newview/llvoicevivox.cpp
@@ -486,7 +486,7 @@ void LLVivoxVoiceClient::connectorCreate()
 	{
 		logpath = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, "");
 	}
-	else if (LLStringUtil::endsWith(logpath, gDirUtilp->getDirDelimiter()))
+	if (LLStringUtil::endsWith(logpath, gDirUtilp->getDirDelimiter()))
 	{
 		logpath = logpath.substr(0, logpath.size() - gDirUtilp->getDirDelimiter().size());
 	}
@@ -834,13 +834,13 @@ void LLVivoxVoiceClient::stateMachine()
                         {
                             log_folder = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, "");
                         }
+                        
 						// <FS:Ansariel> Strip trailing directory delimiter
-						else if (LLStringUtil::endsWith(log_folder, gDirUtilp->getDirDelimiter()))
+						if (LLStringUtil::endsWith(log_folder, gDirUtilp->getDirDelimiter()))
 						{
 							log_folder = log_folder.substr(0, log_folder.size() - gDirUtilp->getDirDelimiter().size());
 						}
 						// </FS:Ansariel>
-                        
 						params.args.add("-lf");
 						params.args.add(log_folder);
 
