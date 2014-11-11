93796447c10cd2a80122efc6fcfe9b271b9f6005
Tank_Master <none@none>
2014-11-10 23:47:15 -0800

    MAINT-3131 code clean-up

--- indra/newview/llglsandbox.cpp
+++ indra/newview/llglsandbox.cpp
@@ -1013,7 +1013,6 @@ F32 gpu_benchmark()
 		gGL.getTexUnit(0)->bindManual(LLTexUnit::TT_TEXTURE, source[i]);
 		LLImageGL::setManualImage(GL_TEXTURE_2D, 0, GL_RGBA, res,res,GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 	}
-	delete [] pixels;
 
     delete [] pixels;
 
