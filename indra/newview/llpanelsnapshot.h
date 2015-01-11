65cbb5b63465d654eb01934507d0561e401e09eb
Ansariel <none@none>
2015-01-11 20:03:32 +0100

    Full sync of snapshot floater with V3 - will need features re-added once the snapshot refactoring is merged in

--- indra/newview/llpanelsnapshot.h
+++ indra/newview/llpanelsnapshot.h
@@ -45,8 +45,6 @@ public:
 	virtual std::string getAspectRatioCBName() const = 0;
 	virtual std::string getImageSizeComboName() const = 0;
 	virtual std::string getImageSizePanelName() const = 0;
-	virtual std::string getTempUploadCBName() const = 0; //FS:LO Fire-6268 [Regression] Temp upload for snapshots missing after FUI merge.
-	virtual std::string getImageSizeControlName() const = 0; // <FS:Zi> Since combo boxes don't save their control variable for some reason, we have to do that manually.
 
 	virtual S32 getTypedPreviewWidth() const;
 	virtual S32 getTypedPreviewHeight() const;
@@ -68,7 +66,6 @@ protected:
 	void onCustomResolutionCommit();
 	void onResolutionComboCommit(LLUICtrl* ctrl);
 	void onKeepAspectRatioCommit(LLUICtrl* ctrl);
-	void onTempUploadCommit(LLUICtrl* ctrl); //FS:LO Fire-6268 [Regression] Temp upload for snapshots missing after FUI merge.
 };
 
 #endif // LL_LLPANELSNAPSHOT_H
