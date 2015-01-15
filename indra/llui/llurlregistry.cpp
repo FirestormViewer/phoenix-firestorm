ab4be0575784126721c1831a8424c4f9bd321012
Ansariel <none@none>
2015-01-15 20:14:55 +0100

    Sync llurlregistry.cpp with V3

--- indra/llui/llurlregistry.cpp
+++ indra/llui/llurlregistry.cpp
@@ -38,7 +38,7 @@ void LLUrlRegistryNullCallback(const std::string &url, const std::string &label,
 
 LLUrlRegistry::LLUrlRegistry()
 {
-//	mUrlEntry.reserve(21);
+//	mUrlEntry.reserve(20);
 // [RLVa:KB] - Checked: 2010-11-01 (RLVa-1.2.2a) | Added: RLVa-1.2.2a
 	mUrlEntry.reserve(22);
 // [/RLVa:KB]
