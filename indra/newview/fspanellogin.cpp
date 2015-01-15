1c6232b88fdf6ea3600757c4ae1b265910170c4f
Ansariel <none@none>
2015-01-15 21:20:46 +0100

    Fix login page not showing home location in combobox anymore

--- indra/newview/fspanellogin.cpp
+++ indra/newview/fspanellogin.cpp
@@ -320,7 +320,7 @@ void FSPanelLogin::addFavoritesToStartLocation()
 	LLComboBox* combo = getChild<LLComboBox>("start_location_combo");
 	if (!combo) return;
 	int num_items = combo->getItemCount();
-	for (int i = num_items - 1; i > 0; i--)
+	for (int i = num_items - 1; i > 2; i--)
 	{
 		combo->remove(i);
 	}
