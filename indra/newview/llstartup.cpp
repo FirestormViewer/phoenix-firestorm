c2ef0bf500497cd47766891340e0b25a8a046237
Ansariel <none@none>
2015-02-17 10:58:27 +0100

    Undo changes to llstartup.cpp that slipped in as part of previous commit...

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -1283,15 +1283,6 @@ bool idle_startup()
 		// Overwrite default user settings with user settings								 
 		LLAppViewer::instance()->loadSettingsFromDirectory("Account");
 
-		LLLayoutStack* chat_bar_stack = gToolBarView->findChild<LLLayoutStack>("chat_bar_stack");
-		if (chat_bar_stack)
-		{
-			LLSD persisted_sizes = //gSavedPerAccountSettings.getLLSD("layout_size_chat_bar_stack");
-				gSavedSettings.getLLSD("layout_size_chat_bar_stack");
-
-			LL_INFOS() << "ADBG: size = " << persisted_sizes.size() << LL_ENDL;
-		}
-
 		// Convert 'LogInstantMessages' into 'KeepConversationLogTranscripts' for backward compatibility (CHUI-743).
 		// <FS:CR> FIRE-11410 - Don't do this, handle it in settings restore and first run
 		//LLControlVariablePtr logInstantMessagesControl = gSavedPerAccountSettings.getControl("LogInstantMessages");
