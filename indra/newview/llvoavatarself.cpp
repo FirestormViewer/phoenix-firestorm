26e18c6448ff36cef236721d2568ba032eeeae4f
Ansariel <none@none>
2015-04-09 11:34:42 +0200

    Make sure we always get the offset right on SL

--- indra/newview/llvoavatarself.cpp
+++ indra/newview/llvoavatarself.cpp
@@ -3308,7 +3308,13 @@ void LLVOAvatarSelf::dumpWearableInfo(LLAPRFile& outfile)
 // [RLVa:KB] - Checked: 2013-03-03 (RLVa-1.4.8)
 F32 LLVOAvatarSelf::getAvatarOffset() /*const*/
 {
-	return (isUsingServerBakes()) ? LLAvatarAppearance::getAvatarOffset() : gSavedPerAccountSettings.getF32("AvatarHoverOffsetZ");
+#ifdef OPENSIM
+	if (!LLGridManager::getInstance()->isInSecondLife())
+	{
+		return (isUsingServerBakes()) ? LLAvatarAppearance::getAvatarOffset() : gSavedPerAccountSettings.getF32("AvatarHoverOffsetZ");
+	}
+#endif
+	return LLAvatarAppearance::getAvatarOffset();
 }
 // [/RLVa:KB]
 
