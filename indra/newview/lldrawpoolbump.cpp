ecbc123b31a891e668d057c9dee4753ccd0c6edd
Nicky <none@none>
2015-04-18 01:44:00 +0200

    Do not leak filehandles...

--- indra/newview/lldrawpoolbump.cpp
+++ indra/newview/lldrawpoolbump.cpp
@@ -129,6 +129,7 @@ void LLStandardBumpmap::addstandard()
 	if( file_version > STD_BUMP_LATEST_FILE_VERSION )
 	{
 		LL_WARNS() << "LLStandardBumpmap has newer version (" << file_version << ") than viewer (" << STD_BUMP_LATEST_FILE_VERSION << ")" << LL_ENDL;
+		fclose( file ); // <FS:ND/> Do not leak the file handle
 		return;
 	}
 
@@ -146,6 +147,7 @@ void LLStandardBumpmap::addstandard()
 		if( fields_read != 2 )
 		{
 			LL_WARNS() << "Bad LLStandardBumpmap entry" << LL_ENDL;
+			fclose( file ); // <FS:ND/> Do not leak the file handle
 			return;
 		}
 
