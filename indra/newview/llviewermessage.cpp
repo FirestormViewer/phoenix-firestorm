e69f06d0996e79976db33f881d2f8ba697ec92a8
Nicky <none@none>
2015-04-22 18:24:12 +0200

    Add word change in llviewermessage.cpp from change f7d7016b591f, this is to be able to import 974f6369f04b.
    --HG--
    branch : Firestorm_4.7.1

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -1477,7 +1477,7 @@ void open_inventory_offer(const uuid_vec_t& objects, const std::string& from_nam
 		////////////////////////////////////////////////////////////////////////////////
 		// Highlight item
 		// <FS:Ansariel> Only show if either ShowInInventory is true OR we use legacy
-		//               accept messages and clicked on the show and the asset is not previewable
+		//               accept messages and clicked on the show button and the asset is not previewable
 		const BOOL auto_open = gSavedSettings.getBOOL("ShowInInventory") || (gSavedSettings.getBOOL("FSUseLegacyInventoryAcceptMessages") && !check_asset_previewable(asset_type));
 			//gSavedSettings.getBOOL("ShowInInventory") && // don't open if showininventory is false
 			//!from_name.empty(); // don't open if it's not from anyone.
