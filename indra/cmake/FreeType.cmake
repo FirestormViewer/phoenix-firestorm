1ff23528d6bc3dde61e791c56683d3d2e6e9f088
Nicky <sl.nicky.ml@googlemail.com>
2015-05-05 18:13:45 +0200

    Add freetype2 to include search path.

--- indra/cmake/FreeType.cmake
+++ indra/cmake/FreeType.cmake
@@ -8,6 +8,7 @@ if (USESYSTEMLIBS)
 else (USESYSTEMLIBS)
   use_prebuilt_binary(freetype)
   set(FREETYPE_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include)
+  set(FREETYPE_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include/freetype2) #<FS:ND/> Also add freetype2 to search dir, or some includes will fail.
   set(FREETYPE_LIBRARIES freetype)
 endif (USESYSTEMLIBS)
 
