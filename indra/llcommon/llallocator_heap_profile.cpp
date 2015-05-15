786e90f006dec53332bcb803ac9deb550444edbd
Nicky <none@none>
2015-05-15 23:52:18 +0200

    Compile fix for Linux x64.

--- indra/llcommon/llallocator_heap_profile.cpp
+++ indra/llcommon/llallocator_heap_profile.cpp
@@ -26,10 +26,10 @@
  */
 
 // <FS:ND> Disable some warnings on newer GCC versions.
-// This might also trigger on something like 4.8, but I did not suchh a GCC to test anything lower than 4.9 and higher than 4.6
+// This might also trigger on something like 4.8, but I did not such a GCC to test anything lower than 4.9 and higher than 4.6
 #if LL_LINUX
+ #pragma GCC diagnostic ignored "-Wuninitialized"
  #if (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__ ) >= 40900
-   #pragma GCC diagnostic ignored "-Wuninitialized"
    #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
  #endif
 #endif
