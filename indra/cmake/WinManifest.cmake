045779c802d873f26a3e2582bdc4451fd9fbc959
Cinder <cinder@sdf.org>
2015-05-20 22:45:15 -0600

    STORM-2105 - Introduct EMBED_MANIFEST cmake macro for Windows and embed in the exes

---
+++ indra/cmake/WinManifest.cmake
@@ -0,0 +1,17 @@
+# - Embeds a specific manifest file in a Windows binary
+# Defines the following:
+# EMBED_MANIFEST - embed manifest in a windows binary with mt
+# Parameters - _target is the target file, type - 1 for EXE, 2 for DLL
+
+  MACRO(EMBED_MANIFEST _target type)
+    ADD_CUSTOM_COMMAND(
+      TARGET ${_target}
+      POST_BUILD
+      COMMAND "mt.exe"
+      ARGS
+        -manifest \"${CMAKE_SOURCE_DIR}\\tools\\manifests\\compatibility.manifest\"
+        -inputresource:\"$<TARGET_FILE:${_target}>\"\;\#${type}
+        -outputresource:\"$<TARGET_FILE:${_target}>\"\;\#${type}
+      COMMENT "Adding compatibility manifest to ${_target}"
+    )
+  ENDMACRO(EMBED_MANIFEST _target)
