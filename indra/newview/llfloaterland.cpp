9b9a688dd4528d94c8e84c08c2f5b30f93e2c869
Ansariel <none@none>
2015-06-29 08:42:23 +0200

    Backed out changeset 18b2e8ff915d (FIRE-16112) as per mail from Oz unless it is stated otherwise

--- indra/newview/llfloaterland.cpp
+++ indra/newview/llfloaterland.cpp
@@ -2134,7 +2134,7 @@ void LLPanelLandOptions::refresh()
 	else
 	{
 		// something selected, hooray!
-		//LLViewerRegion* regionp = LLViewerParcelMgr::getInstance()->getSelectionRegion(); // <FS:LO> FIRE-16112 fix
+		LLViewerRegion* regionp = LLViewerParcelMgr::getInstance()->getSelectionRegion();
 
 		// Display options
 		BOOL can_change_options = LLViewerParcelMgr::isParcelModifiableByAgent(parcel, GP_LAND_OPTIONS);
@@ -2150,7 +2150,7 @@ void LLPanelLandOptions::refresh()
 		mCheckGroupObjectEntry	->set( parcel->getAllowGroupObjectEntry() ||  parcel->getAllowAllObjectEntry());
 		mCheckGroupObjectEntry	->setEnabled( can_change_options && !parcel->getAllowAllObjectEntry() );
 		
-		//BOOL region_damage = regionp ? regionp->getAllowDamage() : FALSE;  // <FS:LO> FIRE-16112 fix
+		BOOL region_damage = regionp ? regionp->getAllowDamage() : FALSE;
 		
 		// <FS:WF> FIRE-6604 : Reinstate the "Allow Other Residents to Edit Terrain" option in About Land
 		BOOL can_change_terraform = LLViewerParcelMgr::isParcelModifiableByAgent(parcel, GP_LAND_EDIT);
@@ -2159,10 +2159,7 @@ void LLPanelLandOptions::refresh()
 		// <FS:WF>	
 		
 		mCheckSafe			->set( !parcel->getAllowDamage() );
-		// <FS:LO> FIRE-16112 fix
-		//mCheckSafe			->setEnabled( can_change_options && region_damage );
-		mCheckSafe			->setEnabled( can_change_options );
-		// </FS:LO>
+		mCheckSafe			->setEnabled( can_change_options && region_damage );
 
 		mCheckFly			->set( parcel->getAllowFly() );
 		mCheckFly			->setEnabled( can_change_options );
@@ -2248,7 +2245,6 @@ void LLPanelLandOptions::refresh()
 			
 			// they can see the checkbox, but its disposition depends on the 
 			// state of the region
-			LLViewerRegion* regionp = LLViewerParcelMgr::getInstance()->getSelectionRegion();  // <FS:LO> FIRE-16112 fix
 			if (regionp)
 			{
 				if (regionp->getSimAccess() == SIM_ACCESS_PG)
