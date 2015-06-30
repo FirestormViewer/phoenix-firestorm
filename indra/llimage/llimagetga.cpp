1d4c678cf3241a1cc34c37de54410a62170d3bb1
Ansariel <none@none>
2015-06-30 16:33:26 +0200

    Fix license header

--- indra/llimage/llimagetga.cpp
+++ indra/llimage/llimagetga.cpp
@@ -17,7 +17,7 @@
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation,s:: Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  * 
  * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
  * $/LicenseInfo$
