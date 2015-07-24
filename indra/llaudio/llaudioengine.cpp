5e3ec29e30abaac8169aa58488f35e6f764b7d09
Ansariel <none@none>
2015-07-24 19:13:18 +0200

    Remove log message that's not needed anymore

--- indra/llaudio/llaudioengine.cpp
+++ indra/llaudio/llaudioengine.cpp
@@ -1975,7 +1975,6 @@ bool LLAudioData::load()
 		// <FS:Ansariel> FIRE-480: Opening multiple instances causes sound failures
 		if (!gDirUtilp->fileExists(wav_path))
 		{
-			LL_WARNS() << "Tried to play sound " << mID.asString() << " which should be cached, but was missing. Trying to re-fetch..." << LL_ENDL;
 			mHasLocalData = false;
 			mHasDecodedData = false;
 			mHasCompletedDecode = false;
