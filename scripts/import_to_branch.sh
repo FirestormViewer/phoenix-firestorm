757d07e2e634e0d511dcaa28ffb3d2145c37be15
Nicky <none@none>
2015-08-07 11:25:30 +0200

    Update for import_to_branch.sh: revert override inis.

--- scripts/import_to_branch.sh
+++ scripts/import_to_branch.sh
@@ -17,6 +17,8 @@ fi
 
 hg revert indra/
 hg revert autobuild.xml
+hg revert package_override_gcc.ini
+hg revert package_override_vc12.ini
 hg purge indra/
 
 for i in $(cat exp.txt|sort -n)
@@ -37,6 +39,8 @@ esac
 
 hg revert indra/
 hg revert autobuild.xml
+hg revert package_override_gcc.ini
+hg revert package_override_vc12.ini
 hg purge indra/
 
 for i in $(cat exp.txt|sort -n)
