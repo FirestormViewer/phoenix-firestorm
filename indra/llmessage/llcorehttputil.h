bd646490c9fcd901ccdd9bad1ee67308d1a8fb25
Nicky <sl.nicky.ml@googlemail.com>
2015-11-04 10:48:49 +0100

    Move FS specific functions into their own files.

--- indra/llmessage/llcorehttputil.h
+++ indra/llmessage/llcorehttputil.h
@@ -677,14 +677,6 @@ private:
 
 } // end namespace LLCoreHttpUtil
 
-namespace FSCoreHttpUtil
-{
-    typedef boost::function<void(const LLSD &)> completionCallback_t;
+#include "fscorehttputil.h"
 
-	void callbackHttpPostRaw(const std::string &url, std::string postData, completionCallback_t success = NULL, completionCallback_t failure = NULL, LLCore::HttpHeaders::ptr_t aHeader = NULL );
-    void callbackHttpGetRaw(const std::string &url, completionCallback_t success = NULL, completionCallback_t failure = NULL, LLCore::HttpHeaders::ptr_t aHeader = NULL);
-
-	void trivialGetCoroRaw(std::string url, LLCore::HttpRequest::policy_t policyId, LLCore::HttpHeaders::ptr_t aHeader, completionCallback_t success, completionCallback_t failure);
-    void trivialPostCoroRaw(std::string url, LLCore::HttpRequest::policy_t policyId, LLCore::BufferArray::ptr_t postData, LLCore::HttpHeaders::ptr_t aHeader, completionCallback_t success, completionCallback_t failure);
-}
 #endif // LL_LLCOREHTTPUTIL_H
