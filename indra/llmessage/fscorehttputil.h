72d4bc92c96c5cff7dc8a40fc6873c8ce595a050
Nicky <sl.nicky.ml@googlemail.com>
2016-04-21 15:36:29 +0200

    OSX could not convert from NULL to  LLCore::HttpHeaders::ptr_t.

--- indra/llmessage/fscorehttputil.h
+++ indra/llmessage/fscorehttputil.h
@@ -3,10 +3,13 @@ namespace FSCoreHttpUtil
 {
     typedef boost::function<void(const LLSD &)> completionCallback_t;
 
-	void callbackHttpPostRaw(const std::string &url, std::string postData, completionCallback_t success = NULL, completionCallback_t failure = NULL, LLCore::HttpHeaders::ptr_t aHeader = NULL );
-    void callbackHttpGetRaw(const std::string &url, completionCallback_t success = NULL, completionCallback_t failure = NULL, LLCore::HttpHeaders::ptr_t aHeader = NULL);
+	void callbackHttpPostRaw(const std::string &url, std::string postData, completionCallback_t success = NULL, completionCallback_t failure = NULL,
+							 LLCore::HttpHeaders::ptr_t aHeader = LLCore::HttpHeaders::ptr_t() );
+    void callbackHttpGetRaw(const std::string &url, completionCallback_t success = NULL, completionCallback_t failure = NULL,
+							LLCore::HttpHeaders::ptr_t aHeader = LLCore::HttpHeaders::ptr_t() );
 
 	void trivialGetCoroRaw(std::string url, LLCore::HttpRequest::policy_t policyId, LLCore::HttpHeaders::ptr_t aHeader, completionCallback_t success, completionCallback_t failure);
-    void trivialPostCoroRaw(std::string url, LLCore::HttpRequest::policy_t policyId, LLCore::BufferArray::ptr_t postData, LLCore::HttpHeaders::ptr_t aHeader, completionCallback_t success, completionCallback_t failure);
+    void trivialPostCoroRaw(std::string url, LLCore::HttpRequest::policy_t policyId, LLCore::BufferArray::ptr_t postData, LLCore::HttpHeaders::ptr_t aHeader,
+							completionCallback_t success, completionCallback_t failure);
 }
 
