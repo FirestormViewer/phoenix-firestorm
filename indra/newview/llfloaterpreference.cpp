6a956039f4855d5a05931c9b9e3030ac28745603
Ansariel <none@none>
2016-04-21 12:00:05 +0200

    FIRE-17518: Opening Preferences from the login screen breaks the login page layout

--- indra/newview/llfloaterpreference.cpp
+++ indra/newview/llfloaterpreference.cpp
@@ -4495,6 +4495,7 @@ void LLPanelPreferenceOpensim::onSelectGrid()
 void LLPanelPreferenceOpensim::apply()
 {
 	LLGridManager::getInstance()->saveGridList();
+	FSPanelLogin::updateServer();
 }
 
 void LLPanelPreferenceOpensim::cancel()
@@ -4596,6 +4597,8 @@ bool LLPanelPreferenceOpensim::removeGridCB(const LLSD& notification, const LLSD
 
 void LLPanelPreferenceOpensim::refreshGridList(bool success)
 {
+	FSPanelLogin::updateServer();
+
 	getChild<LLUICtrl>("grid_management_panel")->setEnabled(TRUE);
 
 	if (!mGridListControl)
