226c2d6d36a6203d2e8f8f383f3952bbd4980853
Ansariel <none@none>
2016-07-12 09:08:20 +0200

    Translation fix

--- indra/newview/rlvhandler.cpp
+++ indra/newview/rlvhandler.cpp
@@ -22,6 +22,7 @@
 #include "llgroupactions.h"
 #include "llhudtext.h"
 #include "llmoveview.h"
+#include "llslurl.h"
 #include "llstartup.h"
 #include "llviewermessage.h"
 #include "llviewermenu.h"
@@ -502,8 +503,8 @@ bool RlvHandler::handleEvent(LLPointer<LLOldEvents::LLEvent> event, const LLSD& 
 
 		if (!s_fGroupChanging)
 		{
-			RlvUtil::notifyBlocked( RLV_STRING_BLOCKED_GROUPCHANGE,
-									LLSD().with( "GROUP_SLURL", (m_idAgentGroup.notNull()) ? llformat("secondlife:///app/group/%s/about", m_idAgentGroup.asString().c_str()) : "(none)"));
+			RlvUtil::notifyBlocked(RLV_STRING_BLOCKED_GROUPCHANGE,
+									LLSD().with("GROUP_SLURL", (m_idAgentGroup.notNull()) ? LLSLURL("group", m_idAgentGroup, "about").getSLURLString() : LLTrans::getString("GroupNameNone")));
 
 			// [Copy/paste from LLGroupActions::activate()]
 			LLMessageSystem* msg = gMessageSystem;
