8b9d7e86bcd39ad02d181f8c92c326fdc4378750
Ansariel <none@none>
2016-10-13 15:29:50 +0200

    FIRE-19540: Add option to log auto-accepted inventory to nearby chat

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -2356,6 +2356,43 @@ void inventory_offer_handler(LLOfferInfo* info)
 			p.payload = payload;
 			LLPostponedNotification::add<LLPostponedOfferNotification>(p, info->mFromID, false);
 		}
+		// <FS:Ansariel> FIRE-19540: Log auto-accepted inventory to nearby chat
+		else if (gSavedSettings.getBOOL("FSLogAutoAcceptInventoryToChat"))
+		{
+			std::string message_type;
+			LLStringUtil::format_map_t chat_args;
+			
+			chat_args["OBJECT_TYPE"] = (!typestr.empty() ? LLTrans::getString(typestr) : "");
+			chat_args["DESC"] = msg;
+
+			if (info->mFromObject)
+			{
+				std::string str_pos;
+				std::string::size_type idx_start = info->mDesc.rfind(" ( http://");
+				std::string::size_type idx_end = info->mDesc.find(" )", idx_start);
+				if (idx_start != std::string::npos && idx_end != std::string::npos)
+				{
+					LLSLURL url_pos(info->mDesc.substr(idx_start + 3, idx_end - (idx_start + 3)));
+					str_pos = "&slurl=" + LLURI::escape(url_pos.getLocationString());
+				}
+
+				chat_args["OBJECT_NAME"] = "[" + LLSLURL("objectim", info->mObjectID, "").getSLURLString() + "?name=" + LLURI::escape(info->mFromName) + "&owner=" + info->mFromID.asString() + (info->mFromGroup ? "&groupowned=true" : "") + str_pos + " " + info->mFromName + "]";
+				message_type = "InvOfferAutoAcceptObject";
+			}
+			else
+			{
+				bool fRlvCanShowName = (!RlvActions::isRlvEnabled()) ||
+					(RlvActions::canShowName(RlvActions::SNC_DEFAULT, info->mFromID)) || (!RlvUtil::isNearbyAgent(info->mFromID)) || (RlvUIEnabler::hasOpenIM(info->mFromID)) || (RlvUIEnabler::hasOpenProfile(info->mFromID));
+				
+				std::string name_slurl = LLSLURL("agent", info->mFromID, (fRlvCanShowName ? "inspect" : "rlvanonym")).getSLURLString();
+
+				chat_args["USER_NAME"] = name_slurl;
+				message_type = "InvOfferAutoAcceptUser";
+			}
+
+			report_to_nearby_chat(LLTrans::getString(message_type, chat_args));
+		}
+		// </FS:Ansariel>
 
 		// <FS:Ansariel> Show offered inventory also if auto-accept is enabled (FIRE-5101)
 		if (bAutoAccept && gSavedSettings.getBOOL("ShowNewInventory"))
