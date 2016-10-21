38374364ee80103f857a996b6be0fc24c0bfa765
Mnikolenko Productengine <mnikolenko@productengine.com>
2016-10-21 17:29:05 +0300

    MAINT-6855 Developer > Show Info > Show Render Info is not reporting accurate vertex counts for Selected Objects

--- indra/newview/llselectmgr.cpp
+++ indra/newview/llselectmgr.cpp
@@ -7508,7 +7508,9 @@ U32 LLObjectSelection::getSelectedObjectTriangleCount(S32* vcount)
 		
 		if (object)
 		{
-			count += object->getTriangleCount(vcount);
+			S32 vt = 0;
+			count += object->getTriangleCount(&vt);
+			*vcount += vt;
 		}
 	}
 
