7fd1d0c9f29c777fa6f003f50678c593bbfa620c
Ansariel <none@none>
2016-11-16 13:51:13 +0100

    Fix missing space in window title

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -6599,7 +6599,7 @@ void LLAppViewer::handleLoginComplete()
 	}
 	if (!full_name.empty())
 	{
-		gWindowTitle += std::string("- ") + full_name;
+		gWindowTitle += std::string(" - ") + full_name;
 		LLStringUtil::truncate(gWindowTitle, 255);
 		gViewerWindow->getWindow()->setTitle(gWindowTitle);
 	}
