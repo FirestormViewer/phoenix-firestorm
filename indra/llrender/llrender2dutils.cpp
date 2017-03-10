3874bf463a81fd6be03d56fa3fc25636883cf68b
Nicky <sl.nicky.ml@googlemail.com>
2017-03-10 20:43:27 +0100

    Correct winding order of second triangle so it does not get culled.

--- indra/llrender/llrender2dutils.cpp
+++ indra/llrender/llrender2dutils.cpp
@@ -1302,8 +1302,8 @@ void gl_rect_2d_simple( S32 width, S32 height )
 		gGL.vertex2i(0, 0);
 
 		gGL.vertex2i(0, 0);
-		gGL.vertex2i(width, height);
 		gGL.vertex2i(width, 0);
+		gGL.vertex2i( width, height );
 	}
 	gGL.end();
 	// </FS:Ansariel>
