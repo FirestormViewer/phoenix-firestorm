8b617a5849bf3677e9da6b0ffc6bf88ca4ee8c3d
Ansariel <none@none>
2017-05-18 08:58:30 +0200

    Remove XUI callback handlers that are not necessary anymore

--- indra/newview/llpanelobjectinventory.cpp
+++ indra/newview/llpanelobjectinventory.cpp
@@ -1657,7 +1657,6 @@ LLPanelObjectInventory::LLPanelObjectInventory(const LLPanelObjectInventory::Par
 	mCommitCallbackRegistrar.add("Inventory.Share",  boost::bind(&LLAvatarActions::shareWithAvatars, this));
 	mCommitCallbackRegistrar.add("Inventory.FileUploadLocation", boost::bind(&do_nothing));
 	mCommitCallbackRegistrar.add("Inventory.CustomAction", boost::bind(&do_nothing)); // <FS:Ansariel> Prevent warning "No callback found for: 'Inventory.CustomAction' in control: Find Links"
-	mCommitCallbackRegistrar.add("Inventory.Purge", boost::bind(&do_nothing)); // <FS:Ansariel> Prevent warning "No callback found for: 'Inventory.Purge' in control: Purge Item"
 }
 
 // Destroys the object
