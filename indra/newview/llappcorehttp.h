da4978540a26b40ac1fb67cf16917c220d251dea
Beq <beqjanus@gmail.com>
2017-09-08 22:43:42 +0100

    Separate Asset and Texture Coro queues to prevent stalling of textures [via Drake]

--- indra/newview/llappcorehttp.h
+++ indra/newview/llappcorehttp.h
@@ -61,6 +61,22 @@ public:
 		/// Pipelined:       no
 		AP_DEFAULT,
 
+		// <FS:Beq> Avoid stall in texture fetch due to asset fetching. [Drake]
+		/// Asset fetching policy class.  Used to
+		/// download assets via capability.
+		/// Deep queueing of requests.
+		/// Do not share.  GET requests only.
+		///
+		/// Destination:     cdn:80
+		/// Protocol:        http:
+		/// Transfer size:   KB-MB
+		/// Long poll:       no
+		/// Concurrency:     high
+		/// Request rate:    high
+		/// Pipelined:       yes
+		AP_ASSET,
+		// </FS:Beq>
+
 		/// Texture fetching policy class.  Used to
 		/// download textures via capability or SSA
 		/// baking service.  Deep queueing of requests.
