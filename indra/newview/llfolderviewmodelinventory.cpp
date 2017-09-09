6c2969791ebde5ba2823e5b083ede01296db43a3
Ansariel <none@none>
2017-09-09 14:02:13 +0200

    FIRE-21632: Update the latest filter generation for empty folders FOR FOLDERS ONLY or it will break filtering of the "Worn" inventory tab

--- indra/newview/llfolderviewmodelinventory.cpp
+++ indra/newview/llfolderviewmodelinventory.cpp
@@ -258,7 +258,10 @@ bool LLFolderViewModelItemInventory::filter( LLFolderViewFilter& filter)
 	{
         // This is where filter check on the item done (CHUI-849)
 		const bool passed_filter = filter.check(this);
-		if (passed_filter && mChildren.empty()) // Update the latest filter generation for empty folders
+		// <FS:Ansariel> FIRE-21632: Only do this for folders or it will break correct filtering of the "Worn" inventory tab
+		//if (passed_filter && mChildren.empty()) // Update the latest filter generation for empty folders
+		if (passed_filter && mChildren.empty() && getInventoryType() == LLInventoryType::IT_CATEGORY) // Update the latest filter generation for empty folders
+		// </FS:Ansariel>
 		{
 			LLFolderViewModelItemInventory* view_model = this;
 			while (view_model && view_model->mMostFilteredDescendantGeneration < filter_generation)
