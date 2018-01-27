b56c2a790eeeae09e70edeabc6a95ba090fc799e
Ansariel <none@none>
2018-01-27 11:43:30 +0100

    Fix Windows build

--- indra/llkdu/llkdumem.h
+++ indra/llkdu/llkdumem.h
@@ -28,7 +28,9 @@
 #define LL_LLKDUMEM_H
 
 //<FS:TS> Silence Clang warning the flag doesn't for some reason
+#if LL_DARWIN
 #pragma clang diagnostic ignored "-Woverloaded-virtual"
+#endif
 //</FS:TS>
 
 // Support classes for reading and writing from memory buffers in KDU
