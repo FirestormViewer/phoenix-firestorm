baffa915c3af0f4cb25390a54c89d7a25630178d
Nicky <sl.nicky.ml@googlemail.com>
2018-04-06 13:16:28 +0200

    Use correct types and casts for OpenGL

--- indra/llrender/llvertexbuffer.cpp
+++ indra/llrender/llvertexbuffer.cpp
@@ -1473,7 +1473,7 @@ void LLVertexBuffer::setupVertexArray()
 				//glVertexattribIPointer requires GLSL 1.30 or later
 				if (gGLManager.mGLSLVersionMajor > 1 || gGLManager.mGLSLVersionMinor >= 30)
 				{
-					glVertexAttribIPointer(i, attrib_size[i], attrib_type[i], sTypeSize[i], (const GLvoid*) mOffsets[i]); 
+				  glVertexAttribIPointer(i, attrib_size[i], attrib_type[i], sTypeSize[i], reinterpret_cast<const GLvoid*>( mOffsets[i] )); 
 				}
 #endif
 			}
