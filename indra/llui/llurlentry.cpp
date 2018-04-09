abb602166f83947aa0dfed39792d59d72403bd69
Ansariel <none@none>
2018-04-10 00:06:16 +0200

    FIRE-22483: Parse HTTPS map SLURL properly - another case

--- indra/llui/llurlentry.cpp
+++ indra/llui/llurlentry.cpp
@@ -350,7 +350,7 @@ LLUrlEntryInvalidSLURL::LLUrlEntryInvalidSLURL()
 {
 	// <FS:Ansariel> Inworldz special
 	//mPattern = boost::regex("(http://(maps.secondlife.com|slurl.com)/secondlife/|secondlife://(/app/(worldmap|teleport)/)?)[^ /]+(/-?[0-9]+){1,3}(/?(\\?title|\\?img|\\?msg)=\\S*)?/?",
-	mPattern = boost::regex("(http://(maps.secondlife.com|slurl.com)/secondlife/|(secondlife|inworldz|iw)://(/app/(worldmap|teleport)/)?)[^ /]+(/-?[0-9]+){1,3}(/?(\\?title|\\?img|\\?msg)=\\S*)?/?",
+	mPattern = boost::regex("(https?://(maps.secondlife.com|slurl.com)/secondlife/|(secondlife|inworldz|iw)://(/app/(worldmap|teleport)/)?)[^ /]+(/-?[0-9]+){1,3}(/?(\\?title|\\?img|\\?msg)=\\S*)?/?",
 									boost::regex::perl|boost::regex::icase);
 	mMenuName = "menu_url_http.xml";
 	mTooltip = LLTrans::getString("TooltipHttpUrl");
