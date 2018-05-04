dffe62b00cd4bca514a8b40d18adeaeebfddfb12
Ansariel <none@none>
2018-05-04 22:41:02 +0200

    And another merge issue

--- indra/newview/llvovolume.cpp
+++ indra/newview/llvovolume.cpp
@@ -4688,13 +4688,6 @@ void LLVolumeGeometryManager::registerFace(LLSpatialGroup* group, LLFace* facep,
 		tex_mat = facep->mTextureMatrix;	
 	}
 
-	// <FS:ND> CEF: if this is a face with media, then use the texture matrix to flip the texture
-	if( facep->mTextureMatrix &&
-		( ( facep->getTextureEntry() && facep->getTextureEntry()->hasMedia() ) ||
-		  ( facep->getTexture() && facep->getTexture()->getType()  == LLViewerTexture::MEDIA_TEXTURE ) ) )
-		tex_mat = facep->mTextureMatrix;
-	// </FS:ND>
-	
 	const LLMatrix4* model_mat = NULL;
 
 	LLDrawable* drawable = facep->getDrawable();
