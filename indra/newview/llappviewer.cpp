826c388b19769b9b529d37312491411176205bdb
Ansariel <none@none>
2018-10-11 23:57:49 +0200

    FIRE-23127: Fix FSEnableLogThrottle not working properly, patch by animats

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -2842,9 +2842,6 @@ bool LLAppViewer::initConfiguration()
 	}
 	//</FS:Techwolf Lupindo>
 
-	// <FS:Ansariel> Debug setting to disable log throttle
-	nd::logging::setThrottleEnabled(gSavedSettings.getBOOL("FSEnableLogThrottle"));
-
 	initStrings(); // setup paths for LLTrans based on settings files only
 	// - set procedural settings
 	// Note: can't use LL_PATH_PER_SL_ACCOUNT for any of these since we haven't logged in yet
@@ -3006,6 +3003,8 @@ bool LLAppViewer::initConfiguration()
 	//Todo, find a cleaner way of doing this via the various set_default arguments.
 	loadSettingsFromDirectory("User");
 	
+	// <FS:Ansariel> Debug setting to disable log throttle
+	nd::logging::setThrottleEnabled(gSavedSettings.getBOOL("FSEnableLogThrottle"));
 
 	// - apply command line settings
 	if (! clp.notify())
