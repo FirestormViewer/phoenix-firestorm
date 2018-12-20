30dcf143367d454fce400662de252ebafc6ef5c9
Liny <liny@pinkfox.xyz>
2018-12-20 03:59:33 -0800

    Copy paste fail.

--- indra/newview/lltoolpie.cpp
+++ indra/newview/lltoolpie.cpp
@@ -125,7 +125,7 @@ BOOL LLToolPie::handleMouseDown(S32 x, S32 y, MASK mask)
 	//left mouse down always picks transparent (but see handleMouseUp)
 	mPick = gViewerWindow->pickImmediate(x, y, TRUE, pick_rigged);
     //LL_INFOS() << "pick_rigged is " << (S32) pick_rigged << " pick time elapsed " << pick_timer.getElapsedTimeF32() << LL_ENDL; // </FS:LO> Clean up log spam left over from SL-713, MAINT-7709.
-	LL_DEBUGS() << "pick_rigged is " << (S32) pick_rigged << " pick time elapsed " << pick_timer.getElapsedTimeF32()
+	LL_DEBUGS() << "pick_rigged is " << (S32) pick_rigged << " pick time elapsed " << pick_timer.getElapsedTimeF32() << LL_ENDL;
 	mPick.mKeyMask = mask;
 
 	mMouseButtonDown = true;
