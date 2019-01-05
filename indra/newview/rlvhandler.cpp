68d943b49ddc53541d02e191f6f02a585b78d8e1
Kitty Barnett <develop@catznip.com>
2019-01-05 11:52:58 +0100

    [FIXED] @setoverlay_alpha causes a diagonal line to appear on the rendered texture   -> Rendering order was off FS vs due to the change from quad -> trig there

--- indra/newview/rlvhandler.cpp
+++ indra/newview/rlvhandler.cpp
@@ -3698,21 +3698,7 @@ void RlvHandler::renderOverlay()
 		const LLVector3 overlayTint = RlvBehaviourDictionary::instance().getModifier(RLV_MODIFIER_OVERLAY_TINT)->getValue<LLVector3>();
 		gGL.color4f(overlayTint.mV[0], overlayTint.mV[1], overlayTint.mV[2], llclamp(RlvBehaviourDictionary::instance().getModifier(RLV_MODIFIER_OVERLAY_ALPHA)->getValue<float>(), 0.0f, 1.0f));
 
-		gGL.begin(LLRender::TRIANGLE_STRIP);
-			gGL.texCoord2f(1.f, 1.f);
-			gGL.vertex2i(nWidth, nHeight);
-			gGL.texCoord2f(0.f, 1.f);
-			gGL.vertex2i(0, nHeight);
-			gGL.texCoord2f(0.f, 0.f);
-			gGL.vertex2i(0, 0);
-
-			gGL.texCoord2f(1.f, 1.f);
-			gGL.vertex2i(nWidth, nHeight);
-			gGL.texCoord2f(0.f, 0.f);
-			gGL.vertex2i(0, 0);
-			gGL.texCoord2f(1.f, 0.f);
-			gGL.vertex2i(nWidth, 0);
-		gGL.end();
+		gl_rect_2d_simple_tex(nWidth, nHeight);
 
 		gGL.getTexUnit(0)->unbind(LLTexUnit::TT_TEXTURE);
 
