da365a8357134e23201415fd8f798b2b6cbefb3f
Nicky <sl.nicky.ml@googlemail.com>
2019-03-04 21:13:55 +0100

    Linux; correct include statement for standalone & remove VLC

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -140,10 +140,12 @@
 #include "stringize.h"
 #include "llcoros.h"
 #include "llexception.h"
-//#if !LL_LINUX
+#if ! (LL_LINUX && LL_USESYSTEMLIBS)
 #include "cef/dullahan.h"
 #include "vlc/libvlc_version.h"
-//#endif // LL_LINUX
+#else
+#include "dullahan.h"
+#endif
 
 // Third party library includes
 #include <boost/bind.hpp>
@@ -3871,7 +3873,7 @@ LLSD LLAppViewer::getViewerInfo() const
 //	info["LIBCEF_VERSION"] = "Undefined";
 //#endif
 
-//#if !LL_LINUX
+#if !(LL_LINUX && LL_USESYSTEMLIBS)
 	std::ostringstream vlc_ver_codec;
 	vlc_ver_codec << LIBVLC_VERSION_MAJOR;
 	vlc_ver_codec << ".";
@@ -3879,9 +3881,9 @@ LLSD LLAppViewer::getViewerInfo() const
 	vlc_ver_codec << ".";
 	vlc_ver_codec << LIBVLC_VERSION_REVISION;
 	info["LIBVLC_VERSION"] = vlc_ver_codec.str();
-//#else
-//	info["LIBVLC_VERSION"] = "Undefined";
-//#endif
+#else
+	info["LIBVLC_VERSION"] = "Undefined";
+#endif
 
 	S32 packets_in = LLViewerStats::instance().getRecording().getSum(LLStatViewer::PACKETS_IN);
 	if (packets_in > 0)
