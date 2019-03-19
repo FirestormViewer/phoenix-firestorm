abdf415be6c22b4946c860e42fbc67c88ebdb803
Nicky <sl.nicky.ml@googlemail.com>
2019-03-19 13:28:49 +0100

    Make jemalloc default.

--- indra/cmake/jemalloc.cmake
+++ indra/cmake/jemalloc.cmake
@@ -1,9 +1,11 @@
 # -*- cmake -*-
 include(Prebuilt)
 
+set( USE_JEMALLOC ON CACHE BOOL "Ship prebuild jemalloc library with packaged viewer" )
+
 if( USE_JEMALLOC )
   if (USESYSTEMLIBS)
-    message( WARNING "Not implemented" )
+    message( WARNING "Search for jemalloc not implemented for standalone builds" )
   else (USESYSTEMLIBS)
     use_prebuilt_binary(jemalloc)
   endif (USESYSTEMLIBS)
