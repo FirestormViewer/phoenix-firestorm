aa87f75ece2dfb87bf3d68a34186d79aa27c3bd9
Tonya Souther <tonya.souther@gmail.com>
2019-05-31 06:27:10 -0500

    Silence Cmake 3.11+ OpenGl warning.

--- indra/cmake/OpenGL.cmake
+++ indra/cmake/OpenGL.cmake
@@ -3,6 +3,10 @@
 include(Variables)
 include(Prebuilt)
 
+# <FS:TS> Shut cmake up about OpenGL library preference. This can
+# be either LEGACY or GLVND.
+SET(OpenGL_GL_PREFERENCE LEGACY)
+
 if (BUILD_HEADLESS)
   SET(OPENGL_glu_LIBRARY GLU)
   SET(OPENGL_HEADLESS_LIBRARIES OSMesa16 dl GLU)
