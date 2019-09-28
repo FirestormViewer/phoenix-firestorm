c0834a7c4c535bfd7d45ff9bd94eec7a6a7e028d
Tonya Souther <tonya.souther@gmail.com>
2019-09-28 18:51:04 -0500

    Xcode 11 compile fix.

--- indra/llwindow/llopenglview-objc.mm
+++ indra/llwindow/llopenglview-objc.mm
@@ -837,7 +837,9 @@ attributedStringInfo getSegments(NSAttributedString *str)
     [super setMarkedText:aString selectedRange:selectedRange replacementRange:replacementRange];
     if ([aString length] == 0)      // this means Input Widow becomes empty
     {
-        [_window orderOut:_window];     // Close this to avoid empty Input Window
+        //[_window orderOut:_window];     // Close this to avoid empty Input Window
+	// <FS:TS> Xcode 11 compile fix
+        [self.window orderOut:self.window];     // Close this to avoid empty Input Window
     }
 }
 
@@ -861,7 +863,9 @@ attributedStringInfo getSegments(NSAttributedString *str)
         (mKeyPressed >= 0xF700 && mKeyPressed <= 0xF8FF))
     {
         // this is case a) of above comment
-        [_window orderOut:_window];     // to avoid empty Input Window
+	// <FS:TS> Xcode 11 compile fix
+        //[_window orderOut:_window];     // to avoid empty Input Window
+        [self.window orderOut:self.window];     // to avoid empty Input Window
     }
 }
 
