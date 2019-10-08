a4409b155c21c2c2e83d83a94ef4753448c279c5
Kitty Barnett <develop@catznip.com>
2019-10-08 19:34:09 +0200

    [FIXED] FIRE-24230: Login crash when RLV @showloc restricted with no teleport history file
    --HG--
    branch : RLVa

--- indra/newview/rlvui.cpp
+++ indra/newview/rlvui.cpp
@@ -137,9 +137,8 @@ void RlvUIEnabler::onToggleShowLoc()
 		// If the last entry in the persistent teleport history matches the current teleport history entry then we should remove it
 		LLTeleportHistory* pTpHistory = LLTeleportHistory::getInstance();
 		LLTeleportHistoryStorage* pTpHistoryStg = LLTeleportHistoryStorage::getInstance();
-		RLV_ASSERT( (pTpHistory) && (pTpHistoryStg) && (pTpHistory->getItems().size() > 0) && (pTpHistory->getCurrentItemIndex() >= 0) );
 		if ( (pTpHistory) && (pTpHistory->getItems().size() > 0) && (pTpHistory->getCurrentItemIndex() >= 0) &&
-			 (pTpHistoryStg) && (pTpHistory->getItems().size() > 0) )
+			 (pTpHistoryStg) && (pTpHistoryStg->getItems().size() > 0) )
 		{
 			const LLTeleportHistoryItem& tpItem = pTpHistory->getItems().back();
 			const LLTeleportHistoryPersistentItem& tpItemStg = pTpHistoryStg->getItems().back();
