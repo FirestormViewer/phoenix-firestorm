20383ecf8eb667b950eb15a925fc61aacf7f6127
Nat Goodspeed <nat@lindenlab.com>
2019-10-22 16:36:16 -0400

    DRTVWR-476: Mention LLApp::stepFrame() in LLAppViewer::idle()
    which performs "by hand" the same sequence of calls found in stepFrame().
    
    Why not simply call stepFrame()? Hysterical reasons?

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -4641,6 +4641,9 @@ void LLAppViewer::idle()
 	LLFrameTimer::updateFrameTime();
 	LLFrameTimer::updateFrameCount();
 	LLEventTimer::updateClass();
+	// LLApp::stepFrame() performs the above three calls plus mRunner.run().
+	// Not sure why we don't call stepFrame() here, except that LLRunner seems
+	// completely redundant with LLEventTimer.
 	LLNotificationsUI::LLToast::updateClass();
 	LLSmoothInterpolation::updateInterpolants();
 	LLMortician::updateClass();
