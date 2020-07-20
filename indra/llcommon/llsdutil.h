43def53230c8d2fb52ff0b7c6a2ba4fc8bdf6dc5
Nicky Dasmijn <nicky.dasmijn@gmail.com>
2020-07-20 19:24:43 +0200

    Hash needs to be in the boost namespace.

--- indra/llcommon/llsdutil.h
+++ indra/llcommon/llsdutil.h
@@ -558,9 +558,11 @@ LLSD shallow(LLSD value, LLSD filter=LLSD()) { return llsd_shallow(value, filter
 
 } // namespace llsd
 
+namespace boost{
 // Specialization for generating a hash value from an LLSD block. 
 template <>
-struct boost::hash<LLSD>
+/* struct boost:hash<LLSD> */
+struct hash<LLSD>
 {
     typedef LLSD argument_type;
     typedef std::size_t result_type;
@@ -621,5 +623,5 @@ struct boost::hash<LLSD>
         return seed;
     }
 };
-
+}
 #endif // LL_LLSDUTIL_H
