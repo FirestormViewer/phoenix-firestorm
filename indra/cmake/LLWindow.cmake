03e501031a8bb8b10dc7358e1cc245890c644ff5
Nicky <nicky.dasmijn@gmail.com>
2020-08-14 21:12:56 +0200

    Remove unnecessary include directives.

--- indra/cmake/LLWindow.cmake
+++ indra/cmake/LLWindow.cmake
@@ -17,14 +17,9 @@ else (USESYSTEMLIBS)
   if (LINUX)
     use_prebuilt_binary(SDL)
     set (SDL_FOUND TRUE)
-    set (SDL_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/i686-linux)
     set (SDL_LIBRARY SDL directfb fusion direct X11)
   endif (LINUX)
 endif (USESYSTEMLIBS)
-
-if (SDL_FOUND)
-  include_directories(${SDL_INCLUDE_DIR})
-endif (SDL_FOUND)
 
 set(LLWINDOW_INCLUDE_DIRS
     ${GLEXT_INCLUDE_DIR}
