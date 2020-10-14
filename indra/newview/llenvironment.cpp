fbef48c913e4324b7f5330383b08189cf38c5b24
Beq <BeqJanus@vcs.firestormviewer.org>
2020-10-14 12:50:03 +0100

    Allow EEP assets to disable water rendering
     This is the underlying support for manual use, UI to follow

--- indra/newview/llenvironment.cpp
+++ indra/newview/llenvironment.cpp
@@ -1471,6 +1471,31 @@ LLEnvironment::DayInstance::ptr_t LLEnvironment::getSharedEnvironmentInstance()
 void LLEnvironment::updateEnvironment(LLSettingsBase::Seconds transition, bool forced)
 {
     DayInstance::ptr_t pinstance = getSelectedEnvironmentInstance();
+    // <FS:Beq> Auto-disable water rendering via hasEEPWaterDerender
+    // Allow parcel and region owners to disable water rendering for their visitors through an EEP setting
+    // We test for the IMG_TRANSPARENT in the NormalMap channel, this is not a valid normal map so cannot clash with
+    // any valid EEP asset use.It also allows non-FS users to create these assets.
+    auto targetWater = pinstance->getWater();
+    static bool hasEEPWaterDerender{false};
+    if(targetWater && targetWater->getNormalMapID()==IMG_TRANSPARENT)
+    {
+        // This EEP has explicit transparent water so let's disable rendering of water
+        if(LLPipeline::hasRenderTypeControl(LLPipeline::RENDER_TYPE_WATER))
+        {
+            hasEEPWaterDerender = true;// remember that it was us who disabled it.
+            LLPipeline::toggleRenderTypeControl(LLPipeline::RENDER_TYPE_WATER);
+        }
+    }
+    else
+    {
+        // Otherwise re-enable it, only if it was us that disabled it
+        if(hasEEPWaterDerender && !LLPipeline::hasRenderTypeControl(LLPipeline::RENDER_TYPE_WATER))
+        {
+            LLPipeline::toggleRenderTypeControl(LLPipeline::RENDER_TYPE_WATER);
+        }
+        hasEEPWaterDerender = false; // regardless of whether it was disabled it is no longer our problem
+    }
+    // </FS:Beq>
 
     if ((mCurrentEnvironment != pinstance) || forced)
     {
