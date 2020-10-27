ffaa950312af02e408bc1067adb3e95ed049bd3c
Beq <BeqJanus@vcs.firestormviewer.org>
2020-10-27 21:36:40 +0000

    Various fixes and repairs to mesh uploader
    * re-establish proper shading of degenerate triangles in phsyics
    * restore configurable settings
    * fix shader lighting and re-align default base colour to not be overexposed.

--- indra/newview/llmodelpreview.cpp
+++ indra/newview/llmodelpreview.cpp
@@ -73,23 +73,23 @@
 // </AW: opensim-limits>
 
 bool LLModelPreview::sIgnoreLoadedCallback = false;
-
-// Extra configurability, to be exposed later in xml (LLModelPreview probably
-// should become UI control at some point or get split into preview control)
-static const LLColor4 PREVIEW_CANVAS_COL(0.169f, 0.169f, 0.169f, 1.f);
-static const LLColor4 PREVIEW_EDGE_COL(0.4f, 0.4f, 0.4f, 1.0);
-static const LLColor4 PREVIEW_BASE_COL(1.f, 1.f, 1.f, 1.f);
-static const LLColor3 PREVIEW_BRIGHTNESS(0.9f, 0.9f, 0.9f);
-static const F32 PREVIEW_EDGE_WIDTH(1.f);
-static const LLColor4 PREVIEW_PSYH_EDGE_COL(0.f, 0.25f, 0.5f, 0.25f);
-static const LLColor4 PREVIEW_PSYH_FILL_COL(0.f, 0.5f, 1.0f, 0.5f);
-static const F32 PREVIEW_PSYH_EDGE_WIDTH(1.f);
-static const LLColor4 PREVIEW_DEG_EDGE_COL(1.f, 0.f, 0.f, 1.f);
-static const LLColor4 PREVIEW_DEG_FILL_COL(1.f, 0.f, 0.f, 0.5f);
-static const F32 PREVIEW_DEG_EDGE_WIDTH(3.f);
-static const F32 PREVIEW_DEG_POINT_SIZE(8.f);
-static const F32 PREVIEW_ZOOM_LIMIT(10.f);
-
+// <FS:Beq> fix up and restore stuff removed by lab
+// // Extra configurability, to be exposed later in xml (LLModelPreview probably
+// // should become UI control at some point or get split into preview control)
+// static const LLColor4 PREVIEW_CANVAS_COL(0.169f, 0.169f, 0.169f, 1.f);
+// static const LLColor4 PREVIEW_EDGE_COL(0.4f, 0.4f, 0.4f, 1.0);
+// static const LLColor4 PREVIEW_BASE_COL(1.f, 1.f, 1.f, 1.f);
+// static const LLColor3 PREVIEW_BRIGHTNESS(0.9f, 0.9f, 0.9f);
+// static const F32 PREVIEW_EDGE_WIDTH(1.f);
+// static const LLColor4 PREVIEW_PSYH_EDGE_COL(0.f, 0.25f, 0.5f, 0.25f);
+// static const LLColor4 PREVIEW_PSYH_FILL_COL(0.f, 0.5f, 1.0f, 0.5f);
+// static const F32 PREVIEW_PSYH_EDGE_WIDTH(1.f);
+// static const LLColor4 PREVIEW_DEG_EDGE_COL(1.f, 0.f, 0.f, 1.f);
+// static const LLColor4 PREVIEW_DEG_FILL_COL(1.f, 0.f, 0.f, 0.5f);
+// static const F32 PREVIEW_DEG_EDGE_WIDTH(3.f);
+// static const F32 PREVIEW_DEG_POINT_SIZE(8.f);
+// static const F32 PREVIEW_ZOOM_LIMIT(10.f);
+// </FS:Beq>
 const F32 SKIN_WEIGHT_CAMERA_DISTANCE = 16.f;
 
 BOOL stop_gloderror()
@@ -2985,6 +2985,22 @@ BOOL LLModelPreview::render()
     bool physics = mViewOption["show_physics"];
     bool uv_guide = mViewOption["show_uv_guide"]; // <FS:Beq> Add UV guide overlay in mesh preview
 
+    // <FS:Beq> restore things lost by the lab during importer work
+    // Extra configurability, to be exposed later as controls?
+    static LLCachedControl<LLColor4> canvas_col(gSavedSettings, "MeshPreviewCanvasColor");
+    static LLCachedControl<LLColor4> edge_col(gSavedSettings, "MeshPreviewEdgeColor");
+    static LLCachedControl<LLColor4> base_col(gSavedSettings, "MeshPreviewBaseColor");
+    static LLCachedControl<LLColor3> brightness(gSavedSettings, "MeshPreviewBrightnessColor");
+    static LLCachedControl<F32> edge_width(gSavedSettings, "MeshPreviewEdgeWidth");
+    static LLCachedControl<LLColor4> phys_edge_col(gSavedSettings, "MeshPreviewPhysicsEdgeColor");
+    static LLCachedControl<LLColor4> phys_fill_col(gSavedSettings, "MeshPreviewPhysicsFillColor");
+    static LLCachedControl<F32> phys_edge_width(gSavedSettings, "MeshPreviewPhysicsEdgeWidth");
+    static LLCachedControl<LLColor4> deg_edge_col(gSavedSettings, "MeshPreviewDegenerateEdgeColor");
+    static LLCachedControl<LLColor4> deg_fill_col(gSavedSettings, "MeshPreviewDegenerateFillColor");
+    static LLCachedControl<F32> deg_edge_width(gSavedSettings, "MeshPreviewDegenerateEdgeWidth");
+    static LLCachedControl<F32> deg_point_size(gSavedSettings, "MeshPreviewDegeneratePointSize");
+    // </FS:Beq>
+
     S32 width = getWidth();
     S32 height = getHeight();
 
@@ -3009,7 +3025,7 @@ BOOL LLModelPreview::render()
         gGL.pushMatrix();
         gGL.loadIdentity();
 
-        gGL.color4fv(PREVIEW_CANVAS_COL.mV);
+        gGL.color4fv(canvas_col().mV); // <FS:Beq/> restore changes removed by the lab
         gl_rect_2d_simple(width, height);
 
         gGL.matrixMode(LLRender::MM_PROJECTION);
@@ -3199,7 +3215,7 @@ BOOL LLModelPreview::render()
     stop_glerror();
 
     gGL.pushMatrix();
-    gGL.color4fv(PREVIEW_EDGE_COL.mV);
+    gGL.color4fv(edge_col().mV); // <FS:Beq/> restore changes removed by the lab
 
     const U32 type_mask = LLVertexBuffer::MAP_VERTEX | LLVertexBuffer::MAP_NORMAL | LLVertexBuffer::MAP_TEXCOORD0;
 
@@ -3292,21 +3308,21 @@ BOOL LLModelPreview::render()
                                 gGL.getTexUnit(0)->bind(mUVGuideTexture, true);
                             }
                         }
-                        gGL.diffuseColor4fv(PREVIEW_BASE_COL.mV);
+                        gGL.diffuseColor4fv(base_col().mV); // <FS:Beq/> restore changes removed by the lab
 
                     }
                     // </FS:Beq>
                     else
                     {
-                        gGL.diffuseColor4fv(PREVIEW_BASE_COL.mV);
+                        gGL.diffuseColor4fv(base_col().mV); // <FS:Beq/> restore changes removed by the lab
                     }
 
                     buffer->drawRange(LLRender::TRIANGLES, 0, buffer->getNumVerts() - 1, buffer->getNumIndices(), 0);
                     gGL.getTexUnit(0)->unbind(LLTexUnit::TT_TEXTURE);
-                    gGL.diffuseColor4fv(PREVIEW_EDGE_COL.mV);
+                    gGL.diffuseColor4fv(edge_col().mV); // <FS:Beq/> restore changes removed by the lab
                     if (edges)
                     {
-                        gGL.setLineWidth(PREVIEW_EDGE_WIDTH); // <FS> Line width OGL core profile fix by Rye Mutt
+                        glLineWidth(edge_width()); // <FS:Beq/> restore changes removed by the lab
                         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
                         buffer->drawRange(LLRender::TRIANGLES, 0, buffer->getNumVerts() - 1, buffer->getNumIndices(), 0);
                         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -3417,13 +3433,16 @@ BOOL LLModelPreview::render()
                                     LLVertexBuffer* buffer = mVertexBuffer[LLModel::LOD_PHYSICS][model][i];
 
                                     gGL.getTexUnit(0)->unbind(LLTexUnit::TT_TEXTURE);
-                                    gGL.diffuseColor4fv(PREVIEW_PSYH_FILL_COL.mV);
+                                    gGL.diffuseColor4fv(phys_fill_col().mV); // <FS:Beq/> restore changes removed by the lab
 
                                     buffer->setBuffer(type_mask & buffer->getTypeMask());
                                     buffer->drawRange(LLRender::TRIANGLES, 0, buffer->getNumVerts() - 1, buffer->getNumIndices(), 0);
-
-                                    gGL.diffuseColor4fv(PREVIEW_PSYH_EDGE_COL.mV);
-                                    gGL.setLineWidth(PREVIEW_PSYH_EDGE_WIDTH); // <FS> Line width OGL core profile fix by Rye Mutt
+                                    // <FS:Beq> restore changes removed by the lab
+                                    // gGL.diffuseColor4fv(PREVIEW_PSYH_EDGE_COL.mV);
+                                    // gGL.setLineWidth(PREVIEW_PSYH_EDGE_WIDTH); // <FS> Line width OGL core profile fix by Rye Mutt
+                                    gGL.diffuseColor4fv(phys_edge_col().mV);
+                                    glLineWidth(phys_edge_width());
+                                    // </FS:Beq> 
                                     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
                                     buffer->drawRange(LLRender::TRIANGLES, 0, buffer->getNumVerts() - 1, buffer->getNumIndices(), 0);
 
@@ -3438,14 +3457,19 @@ BOOL LLModelPreview::render()
                     // only do this if mDegenerate was set in the preceding mesh checks [Check this if the ordering ever breaks]
                     if (mHasDegenerate)
                     {
-                        gGL.setLineWidth(PREVIEW_DEG_EDGE_WIDTH); // <FS> Line width OGL core profile fix by Rye Mutt
-                        glPointSize(PREVIEW_DEG_POINT_SIZE);
-                        // <FS:Beq/> make degenerates show in RED as intended how do we get these red and fullbright is a good question..
+                        // <FS:Beq> restore older functionality lost in lab importer
+                        // gGL.setLineWidth(PREVIEW_DEG_EDGE_WIDTH); // <FS> Line width OGL core profile fix by Rye Mutt
+                        // glPointSize(PREVIEW_DEG_POINT_SIZE);
                         // gPipeline.enableLightsFullbright();
+                        gGL.setLineWidth(deg_edge_width());
+                        glPointSize(deg_point_size());
+                        // gPipeline.enableLightsFullbright(); // This may need to be restored when I fined the cause of the black rendering
+                        // </FS:Beq>
                         //show degenerate triangles
                         LLGLDepthTest depth(GL_TRUE, GL_TRUE, GL_ALWAYS);
                         LLGLDisable cull(GL_CULL_FACE);
-                        gGL.diffuseColor4f(1.f, 0.f, 0.f, 1.f);
+                        
+                        // gGL.diffuseColor4f(1.f, 0.f, 0.f, 1.f); // <FS:Beq/> restore proper functionality
                         const LLVector4a scale(0.5f);
 
                         for (LLMeshUploadThread::instance_list::iterator iter = mUploadData.begin(); iter != mUploadData.end(); ++iter)
@@ -3501,6 +3525,14 @@ BOOL LLModelPreview::render()
 
                                             if (ll_is_degenerate(v1, v2, v3))
                                             {
+                                                // <FS:Beq> restore (configurable) coloured overlay
+                                                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+                                                gGL.diffuseColor4fv(deg_fill_col().mV);
+                                                buffer->draw(LLRender::TRIANGLES, 3, i);
+                                                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+                                                gGL.diffuseColor3fv(deg_edge_col().mV);
+                                                gGL.color3fv(deg_edge_col().mV);
+                                                // </FS:Beq>
                                                 buffer->draw(LLRender::LINE_LOOP, 3, i);
                                                 buffer->draw(LLRender::POINTS, 3, i);
                                             }
@@ -3671,8 +3703,12 @@ BOOL LLModelPreview::render()
 
                             if (edges)
                             {
-                                gGL.diffuseColor4fv(PREVIEW_EDGE_COL.mV);
-                                gGL.setLineWidth(PREVIEW_EDGE_WIDTH); // <FS> Line width OGL core profile fix by Rye Mutt
+                                // <FS:Beq> restore behaviour removed by lab
+                                // gGL.diffuseColor4fv(PREVIEW_EDGE_COL.mV);
+                                // gGL.setLineWidth(PREVIEW_EDGE_WIDTH);
+                                gGL.diffuseColor4fv(edge_col().mV);
+                                gGL.setLineWidth(edge_width());
+                                // </FS:Beq>
                                 glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
                                 buffer->draw(LLRender::TRIANGLES, buffer->getNumIndices(), 0);
                                 glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -3748,7 +3784,11 @@ void LLModelPreview::zoom(F32 zoom_amt)
 {
     F32 new_zoom = mCameraZoom + zoom_amt;
     // TODO: stop clamping in render
-    mCameraZoom = llclamp(new_zoom, 1.f, PREVIEW_ZOOM_LIMIT);
+    // <FS:Beq> restore settings control
+    // mCameraZoom = llclamp(new_zoom, 1.f, PREVIEW_ZOOM_LIMIT); 
+    static LLCachedControl<F32> zoom_limit(gSavedSettings, "MeshPreviewZoomLimit");
+    mCameraZoom = llclamp(new_zoom, 1.f, zoom_limit());
+    // </FS:Beq>
 }
 
 void LLModelPreview::pan(F32 right, F32 up)
