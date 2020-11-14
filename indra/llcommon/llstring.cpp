b01197c134aabb3953bb1492b20d7a11e6714b4f
Ansariel <ansariel.hiller@phoenixviewer.com>
2020-11-14 17:40:53 +0100

    Still use the user's locale configured in the OS for number formatting

--- indra/llcommon/llstring.cpp
+++ indra/llcommon/llstring.cpp
@@ -1298,7 +1298,7 @@ void LLStringUtil::formatNumber(std::string& numStr, std::string decimals)
 		// std::locale() throws if the locale is unknown! (EXT-7926)
 		try
 		{
-			strStream.imbue(std::locale(sLocale.c_str()));
+			strStream.imbue(std::locale(""));
 		} catch (const std::exception &)
 		{
 			LL_WARNS_ONCE("Locale") << "Cannot set locale to " << sLocale << LL_ENDL;
@@ -1335,7 +1335,7 @@ void LLStringUtil::formatNumber(std::string& numStr, std::string decimals)
 		// std::locale() throws if the locale is unknown! (EXT-7926)
 		try
 		{
-			strStream.imbue(std::locale(sLocale.c_str()));
+			strStream.imbue(std::locale(""));
 		} catch (const std::exception &)
 		{
 			LL_WARNS_ONCE("Locale") << "Cannot set locale to " << sLocale << LL_ENDL;
