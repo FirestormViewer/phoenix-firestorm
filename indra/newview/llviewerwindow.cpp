c050e2047924457efbdbb4db1e291efea27616c7
Tonya Souther <tonya.souther@gmail.com>
2021-02-15 09:15:05 -0600

    Compile fix.

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -5941,7 +5941,7 @@ BOOL LLViewerWindow::rawSnapshot(LLImageRaw *raw, S32 image_width, S32 image_hei
 		{
 			// <FS:Ansariel> FIRE-15667: 24bit depth maps
 			//U32 color_fmt = type == LLSnapshotModel::SNAPSHOT_TYPE_DEPTH ? GL_DEPTH_COMPONENT : GL_RGBA;
-			U32 color_fmt = (type == LLSnapshotModel::SNAPSHOT_TYPE_DEPTH || LLSnapshotModel::SNAPSHOT_TYPE_DEPTH24) ? GL_DEPTH_COMPONENT : GL_RGBA;
+			U32 color_fmt = (type == LLSnapshotModel::SNAPSHOT_TYPE_DEPTH || type == LLSnapshotModel::SNAPSHOT_TYPE_DEPTH24) ? GL_DEPTH_COMPONENT : GL_RGBA;
 			// </FS:Ansariel>
 			if (scratch_space.allocate(image_width, image_height, color_fmt, true, true))
 			{
