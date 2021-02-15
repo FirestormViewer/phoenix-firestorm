6534014f6e3901fccc4caa30b3b80606f80f34bb
Kitty Barnett <develop@catznip.com>
2021-02-15 20:19:50 +0100

    AMD shader compile error + just have anyone with older drivers/hardware load the explicit one for now

--- indra/newview/app_settings/shaders/class1/deferred/rlvF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/rlvF.glsl
@@ -147,7 +147,7 @@ void main()
 			break;
 		case 2:		// Blur (variable)
 			fragColor = texture2DRect(diffuseRect, fragTC).rgb;
-			fragColor = mix(fragColor, blurVariable(diffuseRect, fragTC, SPHERE_PARAMS.x, BLUR_DIRECTION, effectStrength), effectStrength > 0);
+			fragColor = mix(fragColor, blurVariable(diffuseRect, fragTC, SPHERE_PARAMS.x, BLUR_DIRECTION, effectStrength), int(effectStrength > 0));
 			break;
 		case 3:		// ChromaticAberration
 			fragColor = chromaticAberration(diffuseRect, fragTC, SPHERE_PARAMS.xy, SPHERE_PARAMS.zw, effectStrength);
