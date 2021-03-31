785d9c03e6ba3a89e17581630fd0c9eec4e6e216
Nicky <nicky.dasmijn@gmail.com>
2021-04-01 00:02:23 +0200

    Need to compile against Win7 to use tracy.

--- indra/cmake/Tracy.cmake
+++ indra/cmake/Tracy.cmake
@@ -5,7 +5,7 @@ if (USE_TRACY_PROFILER)
     if (WINDOWS)
         # set(TRACY_LIBRARIES 
         #     ${ARCH_PREBUILT_DIRS_RELEASE}/Tracy.lib
-        add_definitions(-DTRACY_ENABLE=1 -DTRACY_NO_FASTTIMERS)
+        add_definitions(-DTRACY_ENABLE=1 -DTRACY_NO_FASTTIMERS -DWINVER=0x0601 )
         set(TRACY_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/tracy)
     elseif (DARWIN)
         message(FATAL_ERROR "Tracy is not yet implemented in FS for OSX.")
