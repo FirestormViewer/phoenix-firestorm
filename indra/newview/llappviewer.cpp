6989808ee168cc3ff04b001c4e8e4e115c837abf
Ansariel <ansariel.hiller@phoenixviewer.com>
2021-04-13 21:00:41 +0200

    Fix wrong setting name

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -4943,7 +4943,7 @@ bool LLAppViewer::initCache()
     const double disk_cache_percent = gSavedSettings.getF32("DiskCachePercentOfTotal");
     const unsigned int disk_cache_mb = cache_total_size_mb * disk_cache_percent / 100;
     const unsigned int disk_cache_bytes = disk_cache_mb * 1024 * 1024;
-	const bool enable_cache_debug_info = gSavedSettings.getBOOL("EnableDiskCacheDebugInfo");
+	const bool enable_cache_debug_info = gSavedSettings.getBOOL("EnableCacheDebugInfo");
 	// <FS:Ansariel> Don't ignore cache path for asset cache; Moved further down until cache path has been set correctly
 	//const std::string cache_dir = gDirUtilp->getExpandedFilename(LL_PATH_CACHE, cache_dir_name);
 	//LLDiskCache::initParamSingleton(cache_dir, disk_cache_bytes, enable_cache_debug_info);
