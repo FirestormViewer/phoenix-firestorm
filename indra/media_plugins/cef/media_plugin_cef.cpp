a7037461961aceac44549afe32a78a1e1626da6c
Ansariel <ansariel.hiller@phoenixviewer.com>
2021-05-11 21:12:04 +0200

    Revert screwing up the browser background once again...

--- indra/media_plugins/cef/media_plugin_cef.cpp
+++ indra/media_plugins/cef/media_plugin_cef.cpp
@@ -537,7 +537,7 @@ void MediaPluginCEF::receiveMessage(const char* message_string)
 				settings.host_process_path = std::string(cur_dir_str);
 #endif
 				settings.accept_language_list = mHostLanguage;
-				settings.background_color = 0xff282828; // close to Viewer background color
+				settings.background_color = 0xffffffff;
 				settings.cache_enabled = true;
 				settings.root_cache_path = mRootCachePath;
 				settings.cache_path = mCachePath;
