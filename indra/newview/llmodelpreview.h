9f3b23bd05bd9e11d3f707ba06f6726732546fd0
Liny <liny@pinkfox.xyz>
2021-05-28 19:28:10 -0700

    Fix mac and linux builds.

--- indra/newview/llmodelpreview.h
+++ indra/newview/llmodelpreview.h
@@ -200,7 +200,7 @@ public:
     bool mHasDegenerate;
 
 protected:
-    bool LLModelPreview::matchMaterialOrder(LLModel* lod, LLModel* ref, int& refFaceCnt, int& modelFaceCnt );	// <FS:Beq/> FIRE-30965 Cleanup mesh material parsing 
+    bool				matchMaterialOrder(LLModel* lod, LLModel* ref, int& refFaceCnt, int& modelFaceCnt );	// <FS:Beq/> FIRE-30965 Cleanup mesh material parsing 
     static void			loadedCallback(LLModelLoader::scene& scene, LLModelLoader::model_list& model_list, S32 lod, void* opaque);
     static void			stateChangedCallback(U32 state, void* opaque);
 
