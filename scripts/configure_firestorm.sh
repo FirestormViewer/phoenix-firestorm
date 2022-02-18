97a273e241a5778e7599b4714f8a3922ce2bc40b
Nicky <nicky.dasmijn@gmail.com>
2022-02-18 22:31:31 +0100

    Make sure all bugsplat crashes land in one database per channel.

--- scripts/configure_firestorm.sh
+++ scripts/configure_firestorm.sh
@@ -520,7 +520,7 @@ if [ $WANTS_CONFIG -eq $TRUE ] ; then
         CRASH_REPORTING="-DRELEASE_CRASH_REPORTING=ON"
         if [ ! -z $CHANNEL_SIMPLE ]
         then
-            CRASH_REPORTING="$CRASH_REPORTING -DUSE_BUGSPLAT=On -DBUGSPLAT_DB=firestorm_"`echo $CHANNEL_SIMPLE | tr [:upper:] [:lower:] | sed -e 's/_x64//'`
+            CRASH_REPORTING="$CRASH_REPORTING -DUSE_BUGSPLAT=On -DBUGSPLAT_DB=firestorm_"`echo $CHANNEL_SIMPLE | tr [:upper:] [:lower:] | sed -e 's/x64//' | sed -e 's/_//g'`
         fi
     else
         CRASH_REPORTING="-DRELEASE_CRASH_REPORTING:BOOL=OFF"
