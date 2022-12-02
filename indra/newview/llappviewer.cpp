5eff2f5e837368fec43f6d2d8859c74d6acb14b8
Ansariel <ansariel.hiller@phoenixviewer.com>
2022-12-02 19:44:43 +0100

    Change texture decode thread count defaults

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -2578,8 +2578,9 @@ bool LLAppViewer::initThreads()
     // The only configurable thread count right now is ImageDecode
     // The viewer typically starts around 8 threads not including image decode, 
     // so try to leave at least one core free
-    S32 image_decode_count = llclamp(cores - 9, 1, 8);
-    threadCounts["ImageDecode"] = image_decode_count;
+    //S32 image_decode_count = llclamp(cores - 9, 1, 8);
+    S32 image_decode_count = llclamp(cores - 2, 1, 8); // <FS:Ansariel> Make this a bit more sane
+	threadCounts["ImageDecode"] = image_decode_count;
     gSavedSettings.setLLSD("ThreadPoolSizes", threadCounts);
 
 	// Image decoding
