6a40b64d0ac38d9194c19970ec4966ef9b62df70
Zi Ree <zi.ree@phoenixviewer.com>
2023-04-03 18:42:38 +0200

    Make advanced animation information optional and switchable on the fly.

--- indra/newview/llpreviewanim.cpp
+++ indra/newview/llpreviewanim.cpp
@@ -40,13 +40,17 @@
 #include "lluictrlfactory.h"
 #include "lldatapacker.h"
 
+#include "llviewercontrol.h"	// <FS:Zi> Make advanced animation preview optional
+
 extern LLAgent gAgent;
-//const S32 ADVANCED_VPAD = 3; // <FS:Ansariel> Improved animation preview
 
 LLPreviewAnim::LLPreviewAnim(const LLSD& key)
 	: LLPreview( key )
 {
 	mCommitCallbackRegistrar.add("PreviewAnim.Play", boost::bind(&LLPreviewAnim::play, this, _2));
+	// <FS:Zi> Make advanced animation preview optional
+	mCommitCallbackRegistrar.add("PreviewAnim.Expand", boost::bind(&LLPreviewAnim::expand, this, _2));
+	// </FS:Zi>
 }
 
 // virtual
@@ -54,15 +58,15 @@ BOOL LLPreviewAnim::postBuild()
 {
 	childSetCommitCallback("desc", LLPreview::onText, this);
 	getChild<LLLineEditor>("desc")->setPrevalidate(&LLTextValidate::validateASCIIPrintableNoPipe);
-	// <FS:Ansariel> Improved animation preview
-	//getChild<LLTextBox>("adv_trigger")->setClickedCallback(boost::bind(&LLPreviewAnim::showAdvanced, this));
-	//pAdvancedStatsTextBox = getChild<LLTextBox>("AdvancedStats");
 
-    //// Assume that advanced stats start visible (for XUI preview tool's purposes)
-    //pAdvancedStatsTextBox->setVisible(FALSE);
-    //LLRect rect = getRect();
-    //reshape(rect.getWidth(), rect.getHeight() - pAdvancedStatsTextBox->getRect().getHeight() - ADVANCED_VPAD, FALSE);
-	// </FS:Ansariel>
+	// <FS:Zi> Make advanced animation preview optional
+	bool expanded = gSavedSettings.getBOOL("FSAnimationPreviewExpanded");
+
+	getChild<LLView>("advanced_info_panel")->setVisible(!expanded);
+	getChild<LLButton>("btn_expand")->setToggleState(expanded);
+
+	expand(LLSD());
+	// </FS:Zi>
 
 	return LLPreview::postBuild();
 }
@@ -214,42 +218,36 @@ void LLPreviewAnim::onClose(bool app_quitting)
 	}
 }
 
-// <FS:Ansariel> Improved animation preview
-//void LLPreviewAnim::showAdvanced()
-//{
-//    BOOL was_visible =  pAdvancedStatsTextBox->getVisible();
-//
-//    if (was_visible)
-//    {
-//        pAdvancedStatsTextBox->setVisible(FALSE);
-//        LLRect rect = getRect();
-//        reshape(rect.getWidth(), rect.getHeight() - pAdvancedStatsTextBox->getRect().getHeight() - ADVANCED_VPAD, FALSE);
-//    }
-//    else
-//    {
-//        pAdvancedStatsTextBox->setVisible(TRUE);
-//        LLRect rect = getRect();
-//        reshape(rect.getWidth(), rect.getHeight() + pAdvancedStatsTextBox->getRect().getHeight() + ADVANCED_VPAD, FALSE);
-//
-//        LLMotion *motion = NULL;
-//        const LLInventoryItem* item = getItem();
-//        if (item)
-//        {
-//            // if motion exists, will return existing one.
-//            // Needed because viewer can purge motions
-//            motion = gAgentAvatarp->createMotion(item->getAssetUUID());
-//        }
-//
-//        // set text
-//        if (motion)
-//        {
-//            pAdvancedStatsTextBox->setTextArg("[PRIORITY]", llformat("%d", pMotion->getPriority()));
-//            pAdvancedStatsTextBox->setTextArg("[DURATION]", llformat("%.2f", pMotion->getDuration()));
-//            pAdvancedStatsTextBox->setTextArg("[EASE_IN]", llformat("%.2f", pMotion->getEaseInDuration()));
-//            pAdvancedStatsTextBox->setTextArg("[EASE_OUT]", llformat("%.2f", pMotion->getEaseOutDuration()));
-//            pAdvancedStatsTextBox->setTextArg("[IS_LOOP]", (pMotion->getLoop() ? LLTrans::getString("PermYes") : LLTrans::getString("PermNo")));
-//            pAdvancedStatsTextBox->setTextArg("[NUM_JOINTS]", llformat("%d", pMotion->getNumJointMotions()));
-//        }
-//    }
-//}
-// </FS:Ansariel>
+// <FS:Zi> Make advanced animation preview optional
+void LLPreviewAnim::expand(const LLSD& param)
+{
+	LLView* basic_info_panel = getChild<LLView>("basic_info_panel");
+	LLView* advanced_info_panel = getChild<LLView>("advanced_info_panel");
+
+	// I don't get why we can't use getLocalRect().mTop or something similar to get the .top from the XML -Zi
+	S32 height = getRect().getHeight() - basic_info_panel->getRect().mTop;
+	S32 basic_info_height = basic_info_panel->getRect().getHeight();
+	S32 advanced_info_height = advanced_info_panel->getRect().getHeight();
+
+	bool was_expanded = advanced_info_panel->getVisible();
+	advanced_info_panel->setVisible(!was_expanded);
+
+	height += basic_info_height;
+	if (!was_expanded)
+	{
+		height += advanced_info_height;
+	}
+
+	LLRect rect = getRect();
+
+	rect.setLeftTopAndSize(rect.mLeft, rect.mTop, rect.getWidth(), height);
+	reshape(rect.getWidth(), rect.getHeight(), false);
+
+	setRect(rect);
+
+	if(getHost())
+	{
+		getHost()->growToFit(rect.getWidth(), height);
+	}
+}
+// </FS:Zi>
