83c45e8237e6fc6fb46228fd9caad3b0b7d12b74
Pork Chop <mygoditsfullofstars@phoenixviewer.com>
2023-04-04 13:46:06 +1000

    Linux: Minor cmake cleanup in MediaPluginBase, no longer needs glib

--- indra/cmake/MediaPluginBase.cmake
+++ indra/cmake/MediaPluginBase.cmake
@@ -2,11 +2,8 @@
 
 # <FS:ND> Try to find pulse header, if we got them we can use the linux volume catcher
 if (LINUX)
-  include(GLIB)
 
-  include_directories( ${GLIB_INCLUDE_DIRS} )
-
-  foreach( PULSE_FILE pulse/introspect.h pulse/context.h pulse/subscribe.h pulse/glib-mainloop.h )
+  foreach( PULSE_FILE pulse/introspect.h pulse/context.h pulse/subscribe.h )
 	find_path( PULSE_FILE_${PULSE_FILE}_FOUND ${PULSE_FILE} NO_CACHE)
 	if( NOT PULSE_FILE_${PULSE_FILE}_FOUND )
 	  message( "Looking for ${PULSE_FILE} ... not found")
