274a0cdb6aafe3b76aba5baf343a617adc789e79
Beq <beqjanus@gmail.com>
2024-01-07 22:12:12 +0000

    another override point

--- indra/newview/llvovolume.cpp
+++ indra/newview/llvovolume.cpp
@@ -5516,6 +5516,10 @@ void LLVolumeGeometryManager::registerFace(LLSpatialGroup* group, LLFace* facep,
 	const LLMatrix4* model_mat = NULL;
 
 	LLDrawable* drawable = facep->getDrawable();
+	if(!drawable)
+	{
+		return;
+	}
 	
     if (rigged)
     {
@@ -5551,6 +5555,15 @@ void LLVolumeGeometryManager::registerFace(LLSpatialGroup* group, LLFace* facep,
 
     auto* gltf_mat = (LLFetchedGLTFMaterial*)te->getGLTFRenderMaterial();
     llassert(gltf_mat == nullptr || dynamic_cast<LLFetchedGLTFMaterial*>(te->getGLTFRenderMaterial()) != nullptr);
+
+	// <FS:Beq> show legacy when editing the fallback materials.
+	static LLCachedControl<bool> showSelectedinBP(gSavedSettings, "ShowSelectedInBlinnPhong");
+	if( gltf_mat && facep->getViewerObject()->isSelected() && showSelectedinBP )
+	{
+		gltf_mat = nullptr;
+	}
+	// </FS:Beq>
+
     if (gltf_mat != nullptr)
     {
         mat_id = gltf_mat->getHash(); // TODO: cache this hash
