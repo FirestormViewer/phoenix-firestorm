610a0337ed60c844763e4f08df37b5b1157f3dcc
Beq <beqjanus@gmail.com>
2024-06-19 02:00:14 +0100

    Add dullahan to the whitelist for linux and win
    if I can find mac I'll add it later.

--- indra/newview/fsfloaterwhitelisthelper.cpp
+++ indra/newview/fsfloaterwhitelisthelper.cpp
@@ -50,10 +50,14 @@ void FSFloaterWhiteListHelper::populateWhitelistInfo()
     // On windows use exe (not work or RO) directory
     std::string voiceexe_path = gDirUtilp->getExecutableDir();
     gDirUtilp->append(voiceexe_path, "SLVoice.exe");
+    std::string dullahan_path = gDirUtilp->getLLPluginDir();
+    std::string dullahan_exe =  "dullahan_host.exe";
 #elif LL_DARWIN
     // On MAC use resource directory
     std::string voiceexe_path = gDirUtilp->getAppRODataDir();
     gDirUtilp->append(voiceexe_path, "SLVoice");
+    std::string dullahan_path = ""; // ignore dullahan on mac until we can identify it accurately
+    std::string dullahan_exe = "";
 #else
     std::string voiceexe_path = gDirUtilp->getExecutableDir();
     bool usingWine = gSavedSettings.getBOOL("FSLinuxEnableWin64VoiceProxy");
@@ -65,7 +69,11 @@ void FSFloaterWhiteListHelper::populateWhitelistInfo()
     {
         gDirUtilp->append(voiceexe_path, "win64/SLVoice.exe"); // use bundled win64 version
     }
+    std::string dullahan_path = gDirUtilp->getExecutableDir(); // linux keeps dullahan in the bin folder
+    std::string dullahan_exe = "dullahan_host";
 #endif
+
+    gDirUtilp->append(dullahan_path, dullahan_exe);
 
     const std::string slpluginexe_path = gDirUtilp->getLLPluginLauncher();
 
@@ -80,7 +88,10 @@ void FSFloaterWhiteListHelper::populateWhitelistInfo()
         + gDirUtilp->getBaseFileName(voiceexe_path, false) + "\n" // " Voice Binary"
         + voiceexe_path + "\n" // slvoice full path
         + gDirUtilp->getBaseFileName(slpluginexe_path, false) + "\n" // SLPlugin Launcher Binary
-        + slpluginexe_path + "\n"; // SLPlugin Launcher full path
+        + slpluginexe_path + "\n" // SLPlugin Launcher full path
+        + gDirUtilp->getBaseFileName(dullahan_path, false) + "\n" // SLPlugin Launcher Binary
+        + dullahan_path + "\n"
+        ;  
 
     getChild<LLTextEditor>("whitelist_folders_editor")->setText(whitelist_folder_info);
     getChild<LLTextEditor>("whitelist_exes_editor")->setText(whitelist_exe_info);
