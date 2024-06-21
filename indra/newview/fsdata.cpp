22e0c08ec578657c3c98d2eaf62ea0b0d9f6dd75
Ansariel <ansariel.hiller@phoenixviewer.com>
2024-06-21 21:21:16 +0200

    Fix OpenSim build

--- indra/newview/fsdata.cpp
+++ indra/newview/fsdata.cpp
@@ -259,7 +259,7 @@ static void downloadCompleteScript(LLSD const &aData, std::string const &aURL, s
     LLXMLNodePtr xml_root;
     std::string stringData;
     stringData.assign( rawData.begin(), rawData.end() ); // LLXMLNode::parseBuffer wants a U8*, not a const U8*, so need to copy here just to be safe
-    if ( (!LLXMLNode::parseBuffer( reinterpret_cast< U8*> ( &stringData[0] ), stringData.size(), xml_root, NULL)) || (xml_root.isNull()) || (!xml_root->hasName("script_library")) )
+    if ( (!LLXMLNode::parseBuffer( reinterpret_cast< U8*> ( &stringData[0] ), (U32)stringData.size(), xml_root, NULL)) || (xml_root.isNull()) || (!xml_root->hasName("script_library")) )
     {
         LL_WARNS("fsdata") << "Could not read the script library data from "<< aURL << LL_ENDL;
         return;
@@ -274,7 +274,7 @@ static void downloadCompleteScript(LLSD const &aData, std::string const &aURL, s
     else
     {
         LL_INFOS("fsdata") << "Saving " << aFilename << LL_ENDL;
-        outfile.write(  &rawData[0], rawData.size() );
+        outfile.write(  &rawData[0], (S32)rawData.size() );
         outfile.close() ;
     }
 }
