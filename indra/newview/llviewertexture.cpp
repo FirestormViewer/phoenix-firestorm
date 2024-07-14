91a8726ec961e2f09b084768710d284db54bdccf
TommyTheTerrible <81168766+TommyTheTerrible@users.noreply.github.com>
2024-07-14 17:55:30 -0400

    CHANGE: Update mDesiredDiscardBias based on percentage of free VRAM
    Changed calculation of discard bias to use a percentage of free VRAM, beginning at 3/5ths free, to a maximum bias of 6.

--- indra/newview/llviewertexture.cpp
+++ indra/newview/llviewertexture.cpp
@@ -562,23 +562,21 @@ void LLViewerTexture::updateClass()
 
     F64 texture_bytes_alloc = LLImageGL::getTextureBytesAllocated() / 1024.0 / 512.0;
     F64 vertex_bytes_alloc = LLVertexBuffer::getBytesAllocated() / 1024.0 / 512.0;
+    F64 render_bytes_alloc = LLRenderTarget::sBytesAllocated / 1024.0 / 512.0;
 
     // get an estimate of how much video memory we're using
     // NOTE: our metrics miss about half the vram we use, so this biases high but turns out to typically be within 5% of the real number
-    F32 used = (F32)ll_round(texture_bytes_alloc + vertex_bytes_alloc);
+    F32 used = (F32) ll_round(texture_bytes_alloc + vertex_bytes_alloc + render_bytes_alloc);
 
     F32 budget = max_vram_budget == 0 ? gGLManager.mVRAM : max_vram_budget;
 
-    // try to leave half a GB for everyone else, but keep at least 768MB for ourselves
-    F32 target = llmax(budget - 512.f, 768.f);
+    // TommyTheTerrible - Start Bias creep upwards at 3/5ths VRAM used.
+    F32 whatRemains = budget * 0.60f;
+    F32 target      = llmax(budget - whatRemains, 768.f);
 
-    F32 over_pct = llmax((used-target) / target, 0.f);
-    sDesiredDiscardBias = llmax(sDesiredDiscardBias, 1.f + over_pct);
+    F32 over_pct        = llmax((used - target) / target, 0.f);
+    sDesiredDiscardBias = llclamp(1.f + over_pct, 1, 6);
 
-    if (sDesiredDiscardBias > 1.f)
-    {
-        sDesiredDiscardBias -= gFrameIntervalSeconds * 0.01;
-    }
 
     LLViewerTexture::sFreezeImageUpdates = false; // sDesiredDiscardBias > (desired_discard_bias_max - 1.0f);
 }
