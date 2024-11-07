e84d7ac0ef2a6d18fdab34f2e09809b17ee29222
chanayane <chanayane83@gmail.com>
2024-11-07 13:58:07 +0100

    revert disabling texture batch as it causes issues with alpha blend textures

--- indra/newview/llvovolume.cpp
+++ indra/newview/llvovolume.cpp
@@ -6301,10 +6301,7 @@ void LLVolumeGeometryManager::rebuildGeom(LLSpatialGroup* group)
         pbr_mask = pbr_mask | LLVertexBuffer::MAP_EMISSIVE;
     }
 
-//<AS:chanayane> Disable texture batch (as suggested by TommyTheTerrible)
-    //bool batch_textures = LLViewerShaderMgr::instance()->getShaderLevel(LLViewerShaderMgr::SHADER_OBJECT) > 1;
-    bool batch_textures = false;
-//<AS:chanayane>
+    bool batch_textures = LLViewerShaderMgr::instance()->getShaderLevel(LLViewerShaderMgr::SHADER_OBJECT) > 1;
 
     // add extra vertex data for deferred rendering (not necessarily for batching textures)
     if (batch_textures)
