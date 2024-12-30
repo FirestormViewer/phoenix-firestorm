2fbac952c55deaa77428e1cc82a4222dc2121e52
minerjr <minerjrminerjr@hotmail.com>
2024-12-30 11:18:46 -0400

    FIRE-35019 - Add LLHUBNameTag background to floating text and hover highlights
    Added background image based upon the LLHUBNameTag background to LLHUBText
    Added flag to highlight LLHUBText attached to prims that the user hovers the cursor over.
    Added new value to track if user wants backgrounds on all floating text, only the one that is currently highlighted or none.
    Added new Preferences->User Interface->3D World options to disable the Hover over prim to higlight floating text and Show floating text background options. Defaulted to off.
    Added new Floating Text tab to the Preferences->Colors panel to control the opacity of the LLHUBText background, independant of the LLHUBNameTag value
    Added three new persist values into the setting.xml file, FSHudTextShowBackground, FSHudTextBackgroundOpacity and FSHudTextUseHoverHighlight
    
    Fixed up code commit to better follow the standards for Firestorm Viewer as well as re-factor the code to be cleaner and removed some uncessecary code.

--- indra/newview/llhudtext.h
+++ indra/newview/llhudtext.h
@@ -59,11 +59,30 @@ protected:
             mStyle(style),
             mText(text),
             mFont(font)
-        {}
+        // <FS:minerjr> [FIRE-35019] Add LLHUBNameTag background to floating text and hover highlights
+        //{}        
+        {            
+            // Added a bool check to see if the current line is blank (empty, or has only a single space character stored.
+            // There are issues with users using "Hello World \n \n \n \n" as strings which would create a 4 text segments, but only 1 had actual text
+            // and the background would cover all 4, and would not look nice. So store a flag on each segment to see if it is blank, so we don't have to
+            // do the check every frame for every text segment.
+            if (text.length() == 0 || text.find_first_not_of(utf8str_to_wstring(" "), 0) == std::string::npos)
+            {
+                mbIsBlank = true;
+            }
+            else
+            {
+                mbIsBlank = false;
+            }            
+        }
+        // </FS:minerjr> [FIRE-35019]
         F32 getWidth(const LLFontGL* font);
         const LLWString& getText() const { return mText; }
         void clearFontWidthMap() { mFontWidthMap.clear(); }
 
+        // <FS:minerjr> [FIRE-35019] Add LLHUBNameTag background to floating text and hover highlights
+        bool isBlank() { return mbIsBlank; } // Accessor method for checking to see if the current Text Segment is blank
+        // </FS:minerjr> [FIRE-35019]
         LLColor4                mColor;
         LLFontGL::StyleFlags    mStyle;
         const LLFontGL*         mFont;
@@ -71,6 +90,9 @@ protected:
     private:
         LLWString               mText;
         std::map<const LLFontGL*, F32> mFontWidthMap;
+        // <FS:minerjr> [FIRE-35019] Add LLHUBNameTag background to floating text and hover highlights
+        bool                    mbIsBlank; // True if mText length is 0, or only contains " " characters, otherwise false
+        // <FS:minerjr> [FIRE-35019]
     };
 
 public:
@@ -177,7 +199,12 @@ private:
 // [RLVa:KB] - Checked: RLVa-1.0.0
     std::string     mObjText;
 // [/RLVa:KB]
-
+    // <FS:minerjr> [FIRE-35019] Add LLHUBNameTag background to floating text and hover highlights   
+    LLPointer<LLUIImage> mRoundedRectImgp; // Added background rect image from LLHUBNameTag
+    F32             mBackgroundHeight; // Store the actual height of the background image (calculated from the visible text segments)
+    F32             mBackgroundOffsetY; // Store the offset of the top of the first visible text segment
+    F32             mLuminance; // Store the luminance of the text (used to determine if the background should be white or black for higher contrast)
+    // </FS:minerjr> [FIRE-35019]
     static bool    sDisplayText ;
     static std::set<LLPointer<LLHUDText> > sTextObjects;
     static std::vector<LLPointer<LLHUDText> > sVisibleTextObjects;
