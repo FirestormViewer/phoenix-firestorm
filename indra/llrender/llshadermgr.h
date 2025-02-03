f8e11f738a31ccfb32d4b955e0667aa2356c6376
Beq <beqjanus@gmail.com>
2025-02-03 02:07:15 +0000

    Snapshot Frame overlay and composition guides.
    Initial version of the Snapshot Frame include basic shader to manage the overlay,
    integration with main snapshot and flickr floaters.

--- indra/llrender/llshadermgr.h
+++ indra/llrender/llshadermgr.h
@@ -351,6 +351,14 @@ public:
         SMAA_SEARCH_TEX,                    //  "searchTex"
         SMAA_BLEND_TEX,                     //  "blendTex"
 
+        // <FS:Beq> Uniforms for snapshot frame
+        SNAPSHOT_BORDER_COLOR,              // "border_color"
+        SNAPSHOT_BORDER_THICKNESS,          // "border_thickness"
+        SNAPSHOT_GUIDE_COLOR,               // "guide_color"
+        SNAPSHOT_GUIDE_THICKNESS,           // "guide_thickness"
+        SNAPSHOT_GUIDE_STYLE,               // "guide_style"
+        SNAPSHOT_FRAME_RECT,                // "frame_rect"
+        // </FS:Beq>
         END_RESERVED_UNIFORMS
     } eGLSLReservedUniforms;
     // clang-format on
