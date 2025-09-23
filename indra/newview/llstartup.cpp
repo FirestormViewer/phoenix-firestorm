67847009762ca530af7ac38a6eec2c81749ab2f3
Ansariel <ansariel.hiller@phoenixviewer.com>
2025-09-23 10:45:23 +0200

    Fix device selection being empty (prefer FMOD Studio over OpenAL if both are available)

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -1061,6 +1061,8 @@ bool idle_startup()
 #if !LL_WINDOWS
             // if (NULL == getenv("LL_BAD_OPENAL_DRIVER"))
             if (!gAudiop && NULL == getenv("LL_BAD_OPENAL_DRIVER"))
+#else
+            if (!gAudiop)
 #endif // !LL_WINDOWS
             {
                 gAudiop = (LLAudioEngine *) new LLAudioEngine_OpenAL();
