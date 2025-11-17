4020c120b736cd8f22dbe6e8dd318236224b8da3
Beq <beqjanus@gmail.com>
2025-11-17 16:01:54 +0000

    Include GLIB include directories for GStreamer.

--- indra/cmake/GStreamer10Plugin.cmake
+++ indra/cmake/GStreamer10Plugin.cmake
@@ -1,5 +1,6 @@
 # -*- cmake -*-
 include(Prebuilt)
+include(GLIB) # <FS:Beq/> add glib includes
 if (NOT LINUX)
   return()
 endif()
@@ -10,4 +11,12 @@ use_system_binary(gstreamer10)
 
 use_prebuilt_binary(gstreamer10)
 use_prebuilt_binary(libxml2)
-target_include_directories( ll::gstreamer SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include/gstreamer-1.0)
+# <FS:Beq> make sure glib is found
+set(_gstreamer10_include_dirs
+    ${LIBS_PREBUILT_DIR}/include/gstreamer-1.0
+    ${GLIB_INCLUDE_DIRS}
+)
+list(REMOVE_ITEM _gstreamer10_include_dirs "")
+# </FS:Beq>
+
+target_include_directories( ll::gstreamer SYSTEM INTERFACE ${_gstreamer10_include_dirs})
